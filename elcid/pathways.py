5e2cb117d8b9a1266a414a9ec9049befec4f74da
David Miller <david@deadpansincerity.com>
2015-12-16 13:32:12 +0000

    Rename the pathway, and remove most of the steps. Stub the Cerner pathway that we'll need.

--- elcid/pathways.py
+++ elcid/pathways.py
@@ -7,9 +7,53 @@ from opal.models import Patient
 from pathway.pathways import Pathway, Step
 
 
-class BloodCulturePathway(Pathway):
+class AddPatientPathway(Pathway):
     title = "Add Patient"
+    slug = 'add_patient'
+    
+    steps = (
+        Step(
+            template_url="/pathway/templates/find_patient_form.html",
+            controller_class="FindPatientCtrl",
+            title="Find Patient",
+            icon="fa fa-user"
+        ),
+        Step(
+            model=Location,
+            controller_class="BloodCultureLocationCtrl",
+            template_url="/templates/pathway/blood_culture_location.html"
+        ),
+    )
 
+    def save(self, data, user):
+        with transaction.atomic():
+            update_demographics = data["demographics"]
+            hospital_number = update_demographics["hospital_number"]
+            patient, created = Patient.objects.get_or_create(
+                demographics__hospital_number=hospital_number
+            )
+
+            if created:
+                demographics_model = patient.demographics_set.first()
+                for k, v in update_demographics.iteritems():
+                    setattr(demographics_model, k, v)
+                    demographics_model.save()
+
+            if not patient.episode_set.exists():
+                episode = patient.create_episode()
+            else:
+                episode = patient.episode_set.last()
+
+            tagging = data["tagging"]
+            episode.set_tag_names(tagging, user)
+
+            for step in self.get_steps():
+                step.save(episode.id, data, user)
+
+        return episode
+
+
+class CernerDemoPathway(Pathway):
     steps = (
         Step(
             template_url="/pathway/templates/find_patient_form.html",
@@ -33,30 +77,3 @@ class BloodCulturePathway(Pathway):
         ),
         Antimicrobial,
     )
-
-    def save(self, data, user):
-        with transaction.atomic():
-            update_demographics = data["demographics"]
-            hospital_number = update_demographics["hospital_number"]
-            patient, created = Patient.objects.get_or_create(
-                demographics__hospital_number=hospital_number
-            )
-
-            if created:
-                demographics_model = patient.demographics_set.first()
-                for k, v in update_demographics.iteritems():
-                    setattr(demographics_model, k, v)
-                demographics_model.save()
-
-            if not patient.episode_set.exists():
-                episode = patient.create_episode()
-            else:
-                episode = patient.episode_set.last()
-
-            tagging = data["tagging"]
-            episode.set_tag_names(tagging, user)
-
-            for step in self.get_steps():
-                step.save(episode.id, data, user)
-
-        return episode
