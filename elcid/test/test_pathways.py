4b4affe744d0cd78a916aed530a76850bdd9961a
fredkingham <fredkingham@gmail.com>
2016-03-17 14:41:31 +0000

    fix login in tests

--- elcid/test/test_pathways.py
+++ elcid/test/test_pathways.py
@@ -2,16 +2,17 @@ from opal.core.test import OpalTestCase
 from datetime import date
 
 
-class TestPathwayGet(OpalTestCase):
-    def test_get_pathway(self):
-        pass
-
-
 class TestPathwayPost(OpalTestCase):
     url = "/pathway/cernerdemo/save/"
 
+    def setUp(self):
+        self.assertTrue(
+            self.client.login(
+                username=self.user.username, password=self.PASSWORD
+            )
+        )
+
     def test_post_pathway(self):
-        self.client.login(username=self.USERNAME, password=self.PASSWORD)
         test_data = dict(
             demographics=[dict(hospital_number=234, nhs_number=12312)],
             procedure=[dict(date=date.today())]
