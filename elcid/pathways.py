2fd58ca1c7fd5da07d08b10cbd04490770a0f558
fredkingham <fredkingham@gmail.com>
2016-06-17 16:47:35 +0100

    remove blood culture pathway

--- elcid/pathways.py
+++ elcid/pathways.py
@@ -54,32 +54,3 @@ class CernerDemoPathway(UnrolledPathway):
         MultSaveStep(model=MicrobiologyInput),
         Step(api_name="cerner_note", title="Clinical Note", icon="fa fa-envelope", template_url='/templates/pathway/cernerletter.html')
     )
-
-
-class BloodCulturePathway(ModalPathway):
-    display_name = 'Blood Culture'
-    slug = 'blood_culture'
-    template_url = "/templates/pathway/blood_culture_form_base.html"
-
-    steps = (
-        Step(
-            model=BloodCulture,
-            template_url="/templates/pathway/blood_culture_form.html",
-            controller_class="BloodCultureLocationCtrl"
-        ),
-    )
-
-    def save(self, data, user):
-        """
-            we're expecting all blood cultures for the episode
-            in each post, so if they don't exist, delete them
-        """
-        blood_cultures = data.get("blood_culture", [])
-        blood_culture_ids = [d["id"] for d in blood_cultures if d.get("id")]
-        to_remove = self.episode.bloodculture_set.exclude(
-            id__in=blood_culture_ids
-        )
-
-        to_remove.delete()
-
-        return super(BloodCulturePathway, self).save(data, user)
