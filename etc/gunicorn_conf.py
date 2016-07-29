ade8cf8349b91231fb2ec02f1b93ca535b1f16ab
fredkingham <fredkingham@gmail.com>
2016-07-29 15:19:49 +0100

    fix confs

--- etc/gunicorn_conf.py
+++ etc/gunicorn_conf.py
@@ -1,5 +1,5 @@
 bind = "127.0.0.1:4567"
 timeout = 120
-accesslog = "/home/ubuntu/ohc/log/elcid-rfh.access.log"
-errorlog = "/home/ubuntu/ohc/log/elcid-rfh.error.log"
+accesslog = "/usr/local/ohc/log/elcid-rfh.access.log"
+errorlog = "/usr/local/ohc/log/elcid-rfh.error.log"
 workers = 3
