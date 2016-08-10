feb8810e70762e12bd15662d7b0e07ced071cab3
David Miller <david@deadpansincerity.com>
2016-08-10 14:05:55 +0100

    Use lib for gunicorn logs

--- etc/gunicorn_conf.py
+++ etc/gunicorn_conf.py
@@ -1,5 +1,5 @@
 bind = "127.0.0.1:4567"
 timeout = 120
-accesslog = "/usr/local/ohc/log/elcid-rfh.access.log"
-errorlog = "/usr/local/ohc/log/elcid-rfh.error.log"
+accesslog = "/usr/lib/ohc/log/elcid-rfh.access.log"
+errorlog = "/usr/lib/ohc/log/elcid-rfh.error.log"
 workers = 3
