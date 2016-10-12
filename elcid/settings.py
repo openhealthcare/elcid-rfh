32b6006830b9cd6a53af1f2ab4f980976eca05c9
fredkingham <fredkingham@gmail.com>
2016-10-12 14:49:33 +0100

    remove the unneeded elcid flows

--- elcid/settings.py
+++ elcid/settings.py
@@ -257,7 +257,6 @@ OPAL_OPTIONS_MODULE = 'elcid.options'
 OPAL_BRAND_NAME = 'elCID Royal Free Hospital'
 OPAL_LOG_OUT_MINUTES = 15
 OPAL_LOG_OUT_DURATION = OPAL_LOG_OUT_MINUTES*60*1000
-OPAL_FLOW_SERVICE = 'elCIDFlow'
 
 # Do we need this at all ?
 OPAL_EXTRA_HEADER = 'elcid/print_header.html'
