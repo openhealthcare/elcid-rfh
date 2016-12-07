333a699f479a4e64b55e2665db3cc913a4045be7
fredkingham <fredkingham@gmail.com>
2016-12-07 14:37:08 +0000

    adding dummy api data, removing labs from the requirements, commenting out the blood culture work

--- elcid/assets/js/elcid/controllers/bloodculture_pathway_form.js
+++ elcid/assets/js/elcid/controllers/bloodculture_pathway_form.js
@@ -2,35 +2,35 @@ angular.module('opal.controllers').controller('BloodCulturePathwayFormCtrl',
 function( $modal, $q, ngProgressLite, scope, step, episode, BloodCultureFormHelper) {
       "use strict";
 
-      scope.initialise = function(bloodCultures){
-          _.each(bloodCultures, function(bloodCulture){
-            bloodCulture._formHelper = new BloodCultureFormHelper(
-              bloodCulture, scope.metadata
-            );
-          });
-      };
-
-      if(_.isUndefined(scope.editing.blood_culture)){
-        scope.editing.blood_culture = [{}];
-      }
-      else if(!_.isArray(scope.editing.blood_culture)){
-        scope.editing.blood_culture = [scope.editing.blood_culture];
-      }
-
-      scope.addBloodCulture = function(){
-        var newBloodCulture = {};
-        newBloodCulture._formHelper = new BloodCultureFormHelper(newBloodCulture);
-        scope.editing.blood_culture.push(newBloodCulture);
-      };
-
-      scope.preSave = function(editing){
-        _.each(editing.blood_culture, function(bc){
-          _.each(bc.isolates, function(i){
-            delete i._formHelper;
-          })
-          delete bc._formHelper;
-        });
-      };
-
-      scope.initialise(scope.editing.blood_culture);
+      // scope.initialise = function(bloodCultures){
+      //     _.each(bloodCultures, function(bloodCulture){
+      //       bloodCulture._formHelper = new BloodCultureFormHelper(
+      //         bloodCulture, scope.metadata
+      //       );
+      //     });
+      // };
+      //
+      // if(_.isUndefined(scope.editing.blood_culture)){
+      //   scope.editing.blood_culture = [{}];
+      // }
+      // else if(!_.isArray(scope.editing.blood_culture)){
+      //   scope.editing.blood_culture = [scope.editing.blood_culture];
+      // }
+      //
+      // scope.addBloodCulture = function(){
+      //   var newBloodCulture = {};
+      //   newBloodCulture._formHelper = new BloodCultureFormHelper(newBloodCulture);
+      //   scope.editing.blood_culture.push(newBloodCulture);
+      // };
+      //
+      // scope.preSave = function(editing){
+      //   _.each(editing.blood_culture, function(bc){
+      //     _.each(bc.isolates, function(i){
+      //       delete i._formHelper;
+      //     })
+      //     delete bc._formHelper;
+      //   });
+      // };
+      //
+      // scope.initialise(scope.editing.blood_culture);
 });
