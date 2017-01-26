b7b043842022b079d768811ba84b56e8af200bf8
fredkingham <fredkingham@gmail.com>
2017-01-26 12:51:45 +0000

    adding in the api changes and the url changes

--- elcid/api.py
+++ elcid/api.py
@@ -2,8 +2,14 @@ import json
 
 from rest_framework import viewsets
 from rest_framework.response import Response
-from elcid.gloss_api import bulk_create_from_gloss_response
-from opal.core.api import OPALRouter
+from elcid import gloss_api
+from opal.core.api import (
+    OPALRouter, patient_from_pk, LoginRequiredViewset,
+)
+from opal.core.api import router as opal_api_router
+from opal.core.views import _build_json_response
+from opal.core.api import PatientViewSet
+from opal import models
 
 
 class GlossEndpointApi(viewsets.ViewSet):
@@ -11,9 +17,23 @@ class GlossEndpointApi(viewsets.ViewSet):
 
     def create(self, request):
         request_data = json.loads(request.data)
-        bulk_create_from_gloss_response(request_data)
+        gloss_api.bulk_create_from_gloss_response(request_data)
         return Response("ok")
 
+def gloss_api_query_monkey_patch(fn):
+    """
+    Decorator that passes an episode or returns a 404 from pk kwarg.
+    """
+    def query_api_first(self, request, pk=None):
+        patient = models.Patient.objects.get(pk=pk)
+        hospital_number = patient.demographics_set.get().hospital_number
+        gloss_api.patient_query(hospital_number)
+        return fn(self, request, pk=pk)
+    return query_api_first
 
-router = OPALRouter()
-router.register('glossapi', GlossEndpointApi)
+
+PatientViewSet.retrieve = gloss_api_query_monkey_patch(PatientViewSet.retrieve)
+
+
+gloss_router = OPALRouter()
+gloss_router.register('glossapi', GlossEndpointApi)
