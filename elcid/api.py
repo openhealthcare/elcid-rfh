f5081e5c179e6a2c11fbc0ea5286f52e6a7f78d4
fredkingham <fredkingham@gmail.com>
2017-01-26 12:01:58 +0000

    monkey patch the patient get request

--- elcid/api.py
+++ elcid/api.py
@@ -8,6 +8,8 @@ from opal.core.api import (
 )
 from opal.core.api import router as opal_api_router
 from opal.core.views import _build_json_response
+from opal.core.api import PatientViewSet
+from opal import models
 
 
 class GlossEndpointApi(viewsets.ViewSet):
@@ -18,21 +20,20 @@ class GlossEndpointApi(viewsets.ViewSet):
         gloss_api.bulk_create_from_gloss_response(request_data)
         return Response("ok")
 
-
-class RefreshPatientApi(LoginRequiredViewset):
-    """ goes to gloss, updates the patient locally
-        and sends a copy of that patient to the client
+def gloss_api_query_monkey_patch(fn):
     """
-    base_name = "refresh-patient"
-
-    @patient_from_pk
-    def retrieve(self, request, patient):
+    Decorator that passes an episode or returns a 404 from pk kwarg.
+    """
+    def query_api_first(self, request, pk=None):
+        patient = models.Patient.objects.get(pk=pk)
         hospital_number = patient.demographics_set.get().hospital_number
         gloss_api.patient_query(hospital_number)
-        return _build_json_response(patient.to_dict(request.user))
+        return fn(self, request, pk=pk)
+    return query_api_first
+
+
+PatientViewSet.retrieve = gloss_api_query_monkey_patch(PatientViewSet.retrieve)
 
 
 gloss_router = OPALRouter()
 gloss_router.register('glossapi', GlossEndpointApi)
-
-opal_api_router.register('refresh_patient', RefreshPatientApi)
