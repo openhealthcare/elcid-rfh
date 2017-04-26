a9446d0e95762e5b1dada9b2c18d42cb08242d13
fredkingham <fredkingham@gmail.com>
2017-04-26 17:13:05 +0100

    remove the clinical advice defaults

--- elcid/assets/js/elcid/controllers/clinical_advice_form.js
+++ elcid/assets/js/elcid/controllers/clinical_advice_form.js
@@ -6,38 +6,21 @@ angular.module('opal.controllers').controller(
       ){
         "use strict";
 
-        var REASON_FOR_INTERACTION_COOKIE = "patientNotes-reasonForInteraction";
-        var DISCUSSED_WITH_COOKIE = "patientNotes-discussedWith";
-        /*
-        * expects an episode to exist on the scope
-        */
-        function getCopy(item){
-            var copy = item.makeCopy();
-            var defaults = _.clone({
-                reason_for_interaction: $cookies.get(REASON_FOR_INTERACTION_COOKIE),
-                discussed_with: $cookies.get(DISCUSSED_WITH_COOKIE)
-            });
-
-            return _.extend(copy, defaults);
-        }
-
         var self = this;
 
         $q.all([Referencedata, recordLoader]).then(function(datasets){
 
             angular.extend($scope, datasets[0].toLookuplists());
             var item = $scope.episode.newItem("microbiology_input", {column: $rootScope.fields.microbiology_input});
-            self.editing = getCopy(item);
+            self.editing = item.makeCopy();
 
             self.save = function(){
               ngProgressLite.set(0);
               ngProgressLite.start();
-              $cookies.put(REASON_FOR_INTERACTION_COOKIE, self.editing.reason_for_interaction || "");
-              $cookies.put(DISCUSSED_WITH_COOKIE, self.editing.discussed_with || "");
               item.save(self.editing).then(function() {
                   ngProgressLite.done();
                   item = $scope.episode.newItem('microbiology_input', {column: $rootScope.fields.microbiology_input});
-                  self.editing = getCopy(item);
+                  self.editing = item.makeCopy();
               });
             };
         });
