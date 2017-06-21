ec23171760b6c9b4b8c963eafb29338ec702ba2b
fredkingham <fredkingham@gmail.com>
2017-06-21 10:18:38 +0100

    add a comparator

---
+++ elcid/assets/js/elcid/services/date_added_comparator.js
@@ -0,0 +1,10 @@
+angular.module('opal.services').factory('DateCreatedComparator', function($q, $http, $window, $log) {
+    "use strict";
+
+    var episodeDateComparator = function(someEpisode){
+      debugger;
+      return 1;
+    };
+
+    return [episodeDateComparator];
+});
