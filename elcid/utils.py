02382a54c5b2ec2e7194c1fa515d2776b1120e13
fredkingham <fredkingham@gmail.com>
2017-06-27 12:34:39 +0100

    bump to critical

--- elcid/utils.py
+++ elcid/utils.py
@@ -11,7 +11,7 @@ def timing(f):
         ts = time()
         result = f(*args, **kw)
         te = time()
-        logger.info('func: %r %2.4f sec' % (
+        logger.critical('func: %r %2.4f sec' % (
             f.__name__, te-ts
         ))
         return result
