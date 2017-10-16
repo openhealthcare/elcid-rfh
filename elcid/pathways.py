3ea5b7037e33994d3880a22a88f2c29f4cac1a93
fredkingham <fredkingham@gmail.com>
2017-10-16 13:09:51 +0100

    pathway save for blood cultures was not calling delete others

--- elcid/pathways.py
+++ elcid/pathways.py
@@ -8,10 +8,10 @@ from elcid import gloss_api
 from pathway.pathways import (
     RedirectsToPatientMixin,
     Step,
+    MultiModelStep,
     PagePathway,
     WizardPathway,
 )
-from pathway.steps import delete_others
 
 
 class SaveTaggingMixin(object):
@@ -142,7 +142,7 @@ class BloodCulturePathway(PagePathway):
     slug = "blood_culture"
 
     steps = (
-        Step(
+        MultiModelStep(
             template="pathway/blood_culture.html",
             display_name="Blood Culture",
             icon="fa fa-crosshairs",
