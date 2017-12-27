1575d413270dfe6f17ff0e42e9130c6391a17b3b
fredkingham <fredkingham@gmail.com>
2017-12-27 12:44:28 +0000

    change to use the new opal template variable

--- elcid/assets/js/elcid/controllers/procedure_form.js
+++ elcid/assets/js/elcid/controllers/procedure_form.js
@@ -2,27 +2,26 @@ angular.module('opal.controllers').controller(
   'ProcedureFormCtrl', function($scope) {
     "use strict";
     var vm = this;
-    vm.editing = {procedure_type: undefined};
     vm.procedureTypes = ["medical", "surgical"];
     vm.procedures = [];
-    vm.selectedProcedure = "";
-
-    if(!$scope.editing.procedure){
-      $scope.editing.procedure = {};
-    }
-
+    vm.selectedProcedure = $scope.procedure.surgical_procedure || $scope.procedure.medical_procedure;
     vm.procedures = _.union($scope.medicalprocedure_list, $scope.surgicalprocedure_list);
 
     $scope.$watch(angular.bind(vm, function () {
         return this.selectedProcedure;
     }), function(procedureType){
         if(_.contains($scope.medicalprocedure_list, vm.selectedProcedure)){
-            $scope.editing.procedure.medical_procedure = vm.selectedProcedure;
-            $scope.editing.procedure.surgical_procedure = undefined;
+            $scope.procedure.medical_procedure = vm.selectedProcedure;
+            $scope.procedure.surgical_procedure = undefined;
         }
         else if(_.contains($scope.surgicalprocedure_list, vm.selectedProcedure)){
-            $scope.editing.procedure.surgical_procedure = vm.selectedProcedure;
-            $scope.editing.procedure.medical_procedure = undefined;
+            $scope.procedure.surgical_procedure = vm.selectedProcedure;
+            $scope.procedure.medical_procedure = undefined;
+        }
+        else if(!vm.selectedProcedure || !vm.selectedProcedure.length){
+            // if its undefined or an empty string, nuke it
+            $scope.procedure.surgical_procedure = undefined;
+            $scope.procedure.medical_procedure = undefined;
         }
     });
 });
