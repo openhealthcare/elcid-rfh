439536489f3eaf1f1b24385e0c7ec6b32a252b5d
Marcus Baw <marcusbaw@gmail.com>
2018-01-09 14:05:30 +0000

    created stub patient list for TB service

--- apps/tb/patient_lists.py
+++ apps/tb/patient_lists.py
@@ -4,9 +4,13 @@ from opal.core import patient_lists
 
 class TbPatientList(patient_lists.PatientList):
     display_name = 'TB Service Patients'
+    comparator_service = "EpisodeAddedComparator"
 
     queryset = Episode.objects.all
 
     schema = [
-        patient_lists.Column(title='TB', template_path='apps/tb/templates/records/tb.html')
+        patient_lists.Column(
+            title='TB',
+            template_path='apps/tb/templates/records/tb.html'
+        )
     ]
