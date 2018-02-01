dc108e8f16bc950121ded3652ff7da691943e3eb
fredkingham <fredkingham@gmail.com>
2018-02-01 14:53:23 +0000

    called to task

--- intrahospital_api/tasks.py
+++ intrahospital_api/tasks.py
@@ -6,5 +6,5 @@ from celery import shared_task
 @shared_task
 def load(user, patient):
     from intrahospital_api import loader
-    fname = loader.load_patient()
+    fname = loader.load_patient(patient, user)
     return fname
