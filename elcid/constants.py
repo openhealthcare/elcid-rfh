9b116aedb23a37947087880da56e66a2db1de984
Marcus Baw <marcusbaw@gmail.com>
2018-05-22 10:02:32 +0100

    first 100 most common test results categorised

--- elcid/constants.py
+++ elcid/constants.py
@@ -5,15 +5,76 @@ _LAB_TEST_TAGS = {
         "LIVER PROFILE",
         "IMMUNOGLOBULINS",
         "C REACTIVE PROTEIN",
-        "RENAL",
+        "RENAL PROFILE",
+        "CHOLESTEROL/TRIGLYCERIDES",
+        "IRON STUDIES",
+        "HAEMATINICS",
+        "HBA1C", "HBA1C (DCCT)"
+        "CORTISOL AT 9AM",
+        "THYROID FUNCTION TESTS",
+        "B12 AND FOLATE SCREEN",
+        "GLUCOSE",
+        "ESR",
+        "MAGNESIUM",
+        "25-OH VITAMIN D",
+        "PROTEIN ELECTROPHORESIS",
+        "BICARBONATE",
+        "GAMMA-GT"
+
     ],
-    "HAEMATOLOGY": [
-        "FULL BLOOD COUNT", "HAEMATINICS", "HBA1C"
+    "URINE BIOCHEMISTRY": [
+        "OSMALITY",
+        "PROTEIN ELECTROPHORESIS",
+        "URINE PROTEIN/CR RATIO"
     ],
     "ENDOCRINOLOGY": [
-        "CORTISOL AT 9AM", "THYROID FUNCTION TESTS"
+    "CORTISOL AT 9AM",
+    "THYROID FUNCTION TESTS"
     ],
-    "URINE": [
-        "OSMALITY", "PROTEIN ELECTROPHORESIS"
+    # Haematology
+    "HAEMATOLOGY": [
+        "FULL BLOOD COUNT", "FULL BLOOD COUNT RNOH",
+        "CLOTTING SCREEN",
+        "LYMPHOCYTE SUBSETS",
+    ],
+    "TRANSFUSION/HAEMATOLOGY": [
+        "FULL BLOOD GROUP",
+
+    ]
+    # Microbiology
+    "MICROBIOLOGY": [
+        "MRSA SCREEN",
+        "RESPIRATORY PCR",
+        "STOOL CULTURE",
+        "BLOOD CULTURE",
+        "CHLAMYDIA / GC SDA ASSAY",
+        "SYPHILIS SEROLOGY"
+    ],
+    # Immunology
+    "IMMUNOLOGY": [
+        "IMMUNOGLOBULINS",
+        "SERUM LIGHT CHAINS",
+        "IMMUNODEFICIENCY PANEL",
+        "ANA BATTERY"
+    ]
+    "PATHOLOGY": [
+        "HISTOLOGY",
+
+    ]
+    # Misc/Ambiguous
+    "MISC": [
+        "COMMENT",
+        "SAMPLE COMMENT",
+    ]
+    "AMBIGUOUS": [
+        "URINE SCREENING TEST",  # screening for what?
+        "ANTIBODY SCREEN", # this can mean Transfusion antibodies or a more general Immunology screen. Probably 'Immunology' on balance.
+    ],
+
+    # some groupings that might be useful for specific specialities
+    "SPECIALITY-NEPHROLOGY": [
+        "URINE PROTEIN/CR RATIO",
+        "UREA AND ELECTROLYTES",
+        "URINE ALB:CREAT RATIO",
     ],
 }
