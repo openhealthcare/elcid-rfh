207cb54a308498f8225bf0d69c82f5c84451b41d
fredkingham <fredkingham@gmail.com>
2018-08-16 19:36:13 +0100

    bumps the version number and resets the timezone to UTC

--- elcid/settings.py
+++ elcid/settings.py
@@ -330,7 +330,7 @@ else:
     EMAIL_HOST = 'localhost'
 
 
-VERSION_NUMBER = '0.3.5'
+VERSION_NUMBER = '0.3.5.1'
 
 #TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #TEST_RUNNER = 'django_test_coverage.runner.CoverageTestSuiteRunner'
