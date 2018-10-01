5db06db852936a6b8b838d3c02a033b5eada09fe
fredkingham <fredkingham@gmail.com>
2018-10-01 15:19:02 +0100

    hide the lists menu item for tb users refs #487

--- elcid/test/test_application.py
+++ elcid/test/test_application.py
@@ -1,36 +1,53 @@
+from django.contrib.auth.models import User
 from opal.core.test import OpalTestCase
+from opal import models as opal_models
 from elcid import Application
 from apps.tb import constants as tb_constants
 
 
 class ApplicationTestCase(OpalTestCase):
+    PASSWORD = "password"
+
     def setUp(self):
         super(ApplicationTestCase, self).setUp()
-        self.user.is_superuser = False
-        self.user.save()
+        normal_user = User.objects.create(username="normal")
+        normal_user.set_password(self.PASSWORD)
+        normal_user.save()
+        self.normal_user = normal_user
 
-    def test_get_menu_items_superuser(self):
-        self.user.is_superuser = True
-        self.user.save()
-        menu_items = Application.get_menu_items(self.user)
-        expected_hrefs = [menu_item.href for menu_item in menu_items]
-        self.assertIn("/pathway/#/add_patient", expected_hrefs)
-
-    def test_get_menu_items_tb(self):
-        self.user.profile.roles.create(
+        tb_user = User.objects.create(username="tb")
+        tb_user.set_password(self.PASSWORD)
+        tb_user.save()
+        opal_models.UserProfile.objects.create(user=tb_user)
+        tb_user.profile.roles.create(
             name=tb_constants.TB_ROLE
         )
-        menu_items = Application.get_menu_items(self.user)
-        expected_hrefs = [menu_item.href for menu_item in menu_items]
-        self.assertNotIn("/pathway/#/add_patient", expected_hrefs)
+        self.tb_user = tb_user
 
-    def test_get_menu_items_not_tb(self):
-        self.user.profile.roles.create(
-            name=tb_constants.TB_ROLE
-        ).delete()
-        menu_items = Application.get_menu_items(self.user)
+        super_user = User.objects.create(username="super")
+        super_user.set_password(self.PASSWORD)
+        super_user.is_superuser = True
+        super_user.save()
+
+        self.super_user = super_user
+
+    def test_get_menu_items_superuser(self):
+        menu_items = Application.get_menu_items(self.super_user)
         expected_hrefs = [menu_item.href for menu_item in menu_items]
         self.assertIn("/pathway/#/add_patient", expected_hrefs)
+        self.assertIn("/#/list/", expected_hrefs)
+
+    def test_get_menu_items_for_tb_user(self):
+        menu_items = Application.get_menu_items(self.tb_user)
+        expected_hrefs = [menu_item.href for menu_item in menu_items]
+        self.assertNotIn("/pathway/#/add_patient", expected_hrefs)
+        self.assertNotIn("/#/list/", expected_hrefs)
+
+    def test_get_menu_items_for_normal_user(self):
+        menu_items = Application.get_menu_items(self.normal_user)
+        expected_hrefs = [menu_item.href for menu_item in menu_items]
+        self.assertIn("/pathway/#/add_patient", expected_hrefs)
+        self.assertIn("/#/list/", expected_hrefs)
 
     def test_make_sure_we_dont_change_a_global_object(self):
         # make sure we don't change the list as it appears on
