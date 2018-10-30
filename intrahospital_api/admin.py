064f56b6c215b07f16a09369bb8e2a2aa82edee6
fredkingham <fredkingham@gmail.com>
2018-10-30 15:06:08 +0000

    the batch load count should be being updated #479

--- intrahospital_api/admin.py
+++ intrahospital_api/admin.py
@@ -86,7 +86,7 @@ class PatientLoadAdmin(admin.ModelAdmin):
 
 
 class BatchPatientLoadAdmin(PatientLoadAdmin):
-    list_display = ["__str__", "started", "stopped", "state"]
+    list_display = ["__str__", "started", "stopped", "state", "count"]
 
 
 class InitialPatientLoadAdmin(PatientSubrecordAdmin, PatientLoadAdmin):
