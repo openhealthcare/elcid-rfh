c8a9f1ac76f207cc71154b3fe8598729d39f3551
David Miller <david@deadpansincerity.com>
2018-11-14 13:01:53 +0000

    Use full path to local settings

--- elcid/settings.py
+++ elcid/settings.py
@@ -378,6 +378,7 @@ REST_FRAMEWORK = {
 
 if 'test' not in sys.argv:
     try:
-        from local_settings import *
+        from elcid.local_settings import *
     except ImportError:
+        raise
         pass
