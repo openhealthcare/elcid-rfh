bcef9a45243682e1aebb0e9cc80f3138686e46e4
David Miller <david@deadpansincerity.com>
2018-11-27 12:55:57 +0000

    Add opal lab to this repo wholesale to enable refactor/deleting code without lots of repo juggling.

---
+++ lab/views.py
@@ -0,0 +1,38 @@
+"""
+Views for the lab Opal Plugin
+"""
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+
+class AbstractLabTestView(LoginRequiredMixin, TemplateView):
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(AbstractLabTestView, self).get_context_data(*args, **kwargs)
+        ctx["lab_test"] = self.lab_test
+        return ctx
+
+    def dispatch(self, *a, **kw):
+        """
+        Set the context for what this modal is for so
+        it can be accessed by all subsequent methods
+        """
+        from lab.models import LabTest # Don't complain about importing it before the app is loaded
+
+        self.lab_test = LabTest.get_class_from_api_name(kw['model'])
+        return super(AbstractLabTestView, self).dispatch(*a, **kw)
+
+
+class LabTestResultTemplateView(AbstractLabTestView):
+    """
+    This view renders the form template for our lab test.
+    """
+    def get_template_names(self):
+        return self.lab_test.get_result_form()
+
+
+class LabTestRecordTemplateView(AbstractLabTestView):
+    """
+    This view renders the record template for our lab test.
+    """
+    def get_template_names(self):
+        return self.lab_test.get_record()
