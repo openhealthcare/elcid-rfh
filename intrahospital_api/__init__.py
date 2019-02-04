cf0c8015ed11aed6317d282c171ab04f2bdce0b3
fredkingham <fredkingham@gmail.com>
2019-02-04 16:22:23 +0000

    changes elcid to run with python 3 (note deployment will not work)

--- intrahospital_api/__init__.py
+++ intrahospital_api/__init__.py
@@ -3,7 +3,7 @@ Opal core intrahospital api package
 """
 from opal.core import celery  # NOQA
 import logging
-from apis import get_api
+from intrahospital_api.apis import get_api
 
 __all__ = [
     "get_api"
