a8d7a4ccf06d2a90a7f4d7a40732d93dda841ccf
fredkingham <fredkingham@gmail.com>
2019-03-06 18:45:21 +0000

    an initial go at the tb letters template

---
+++ apps/tb/api.py
@@ -0,0 +1,16 @@
+from collections import OrderedDict
+from elcid.api import AbstractLabTestSummaryApi
+
+
+class TBTestSummary(AbstractLabTestSummaryApi):
+    base_name = "tb_test_summary"
+
+    RELEVANT_TESTS = OrderedDict((
+        ("C REACTIVE PROTEIN", "C Reactive Protein"),
+        ('LIVER PROFILE', ['ALT', 'AST', 'Total Bilirubin']),
+        ('HEPATITIS B SURFACE AG', ["Hepatitis B 's'Antigen........"]),
+        ('HEPATITIS C ANTIBODY', ["Hepatitis C IgG Antibody......"]),
+        ('HIV 1 + 2 ANTIBODIES', 'HIV 1 + 2 Antibodies..........'),
+    ))
+
+
