23d72a6c8515d4ba846674fa2ed732786ead0a54
fredkingham <fredkingham@gmail.com>
2019-03-07 17:33:21 +0000

    removes the incorrect doc string of the letters index view

--- plugins/letters/views.py
+++ plugins/letters/views.py
@@ -7,10 +7,5 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-
 class LettersIndexView(LoginRequiredMixin, TemplateView):
-    """
-    Main entrypoint into the pathway portal service.
-    This is the entry point that loads in the pathway.
-    """
     template_name = "letters/index.html"
