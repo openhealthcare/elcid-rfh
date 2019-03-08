1996086988a837d7030adf29ac2673270e62ea6a
David Miller <david@deadpansincerity.com>
2019-03-08 11:24:29 +0000

    remove old scaffold cruft

--- plugins/letters/plugin.py
+++ plugins/letters/plugin.py
@@ -21,16 +21,3 @@ class LettersPlugin(plugins.OpalPlugin):
     stylesheets = [
         "css/letters.css"
     ]
-
-    def list_schemas(self):
-        """
-        Return any patient list schemas that our plugin may define.
-        """
-        return {}
-
-    def roles(self, user):
-        """
-        Given a (Django) USER object, return any extra roles defined
-        by our plugin.
-        """
-        return {}
