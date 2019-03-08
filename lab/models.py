e3016749505f1e3fac6b2c4fb35378c12c4662c4
fredkingham <fredkingham@gmail.com>
2019-03-08 13:25:39 +0000

    An initial commit of the remodelling of lab tests

--- lab/models.py
+++ lab/models.py
@@ -1,5 +1,6 @@
 import six
 from jsonfield import JSONField
+from opal.core import serialization
 from django.db import models
 from django.db import transaction
 from django.urls import reverse
@@ -652,3 +653,100 @@ class ReadOnlyLabTest(LabTest, AbstractBase):
         result = super(ReadOnlyLabTest, self).to_dict(*args, **kwargs)
         result["observations"] = result["extras"].pop("observations", {})
         return result
+
+
+class Test(models.Model):
+    patient = models.ForeignKey(omodels.Patient, on_delete=models.CASCADE)
+    clinical_info = models.TextField(blank=True)
+    datetime_ordered = models.DateTimeField(null=True, blank=True)
+    site = models.CharField(max_length=256, blank=True, null=True)
+    status = models.CharField(max_length=256, blank=True, null=True)
+    test_code = models.CharField(max_length=256, blank=True, null=True)
+    test_name = models.CharField(max_length=256, blank=True, null=True)
+    result_id = models.CharField(max_length=256, blank=True, null=True)
+
+    @classmethod
+    def create(cls, lt):
+        s = cls()
+        s.patient = lt.patient
+        extras = lt.extras
+        fields = [
+            "clinical_info",
+            "site",
+            "status",
+            "test_code",
+            "test_name",
+        ]
+        for f in fields:
+            setattr(s, f, extras.get(f, None))
+        s.datetime_ordered = lt.datetime_ordered
+        s.result_id = lt.external_identifier
+        s.save()
+        for i in extras["observations"]:
+            Obs.create(s, i)
+        return s
+
+    @property
+    def extras(self):
+        fields = [
+            "clinical_info",
+            "site",
+            "status",
+            "test_code",
+            "test_name",
+        ]
+        result = {}
+        for field in fields:
+            result[field] = getattr(self, field)
+
+        result["observations"] = {
+            i.to_dict() for i in self.observation_set.all()
+        }
+        return result
+
+
+class Obs(models.Model):
+    last_updated = models.DateTimeField(blank=True, null=True)
+    observation_datetime = models.DateTimeField(blank=True, null=True)
+    observation_name = models.CharField(max_length=256, blank=True, null=True)
+    observation_number = models.CharField(max_length=256, blank=True, null=True)
+    observation_value = models.TextField(blank=True)
+    reference_range = models.CharField(max_length=256, blank=True, null=True)
+    units = models.CharField(max_length=256, blank=True, null=True)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+
+    @classmethod
+    def create(cls, test, observation_dict):
+        o = cls()
+        o.test = test
+        o.last_updated = serialization.deserialize_datetime(
+            observation_dict["last_updated"]
+        )
+        o.observation_datetime = serialization.deserialize_datetime(
+            observation_dict["observation_datetime"]
+        )
+        fields = [
+            "observation_number",
+            "observation_name",
+            "observation_value",
+            "reference_range",
+            "units"
+        ]
+        for f in fields:
+            setattr(o, f, observation_dict.get(f))
+        o.save()
+        return o
+
+    def to_dict(self):
+        fields = self._meta.get_fields()
+        fields = [i for i in fields if i not in {"id", "test"}]
+        return {i: getattr(self, i) for i in fields}
+
+
+def create_test():
+    ut = LabTest.objects.filter(lab_test_type__istartswith="up").last()
+    return Test.create(ut)
+
+def create_tests():
+    for ut in LabTest.objects.filter(lab_test_type__istartswith="up"):
+        Test.create(ut)
