9085ff6eae43b8f7e0551f2388d6dc7f2d914fc6
fredkingham <fredkingham@gmail.com>
2019-03-09 13:20:46 +0000

    Adds/corrects the name of the tb observations in the results panel

--- apps/tb/utils.py
+++ apps/tb/utils.py
@@ -6,7 +6,8 @@ RELEVANT_TESTS = OrderedDict((
     ("LIVER PROFILE", ["ALT", "AST", "Total Bilirubin"]),
     ("QUANTIFERON TB GOLD IT", [
         "QFT IFN gamma result (TB1)",
-        "QFT IFN gamma result (TB2)",
+        "QFT IFN gamme result (TB2)",
+        "QFT TB interpretation"
     ]),
     ('HEPATITIS B SURFACE AG', ["Hepatitis B 's'Antigen........"]),
     ('HEPATITIS C ANTIBODY', ["Hepatitis C IgG Antibody......"]),
