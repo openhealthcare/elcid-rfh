d67dd9482892d64361ddebdae8f8d43f6b37f397
fredkingham <fredkingham@gmail.com>
2019-03-18 22:53:48 +0000

    changes the intrahospital api to update the new lab tests models

---
+++ intrahospital_api/test/test_update_lab_tests.py
@@ -0,0 +1,132 @@
+import datetime
+from django.utils import timezone
+from opal.core.test import OpalTestCase
+from intrahospital_api import update_lab_tests
+from plugins.labtests import models as lab_test_models
+
+
+class TestGetOrCreateLabTest(OpalTestCase):
+    def setUp(self):
+        self.api_dict = {
+            "clinical_info":  'testing',
+            "datetime_ordered": "17/07/2015 04:15:10",
+            "external_identifier": "11111",
+            "site": u'^&        ^',
+            "status": "Success",
+            "test_code": "AN12",
+            "test_name": "Anti-CV2 (CRMP-5) antibodies",
+            "observations": [{
+                "last_updated": "18/07/2015 04:15:10",
+                "observation_datetime": "19/07/2015 04:15:10",
+                "observation_name": "Aerobic bottle culture",
+                "observation_number": "12312",
+                "observation_value": "123",
+                "reference_range": "3.5 - 11",
+                "units": "g"
+            }]
+        }
+        self.patient, _ = self.new_patient_and_episode_please()
+
+
+    def test_creates_lab_test(self):
+        lt, created = update_lab_tests.get_or_create_lab_test(
+            self.patient, self.api_dict
+        )
+        self.assertTrue(created)
+
+        self.assertEqual(
+            lt.patient, self.patient
+        )
+        self.assertEqual(
+            lt.clinical_info, 'testing'
+        )
+        self.assertEqual(
+            lt.datetime_ordered,
+            datetime.datetime(
+                2015, 7, 17, 4, 15, 10
+            )
+        )
+        self.assertEqual(
+            lt.lab_number, '11111'
+        )
+        self.assertEqual(
+            lt.status, 'Success'
+        )
+        self.assertEqual(
+            lt.test_code, 'AN12'
+        )
+        self.assertEqual(
+            lt.test_name, 'Anti-CV2 (CRMP-5) antibodies'
+        )
+
+        self.assertEqual(
+            lt.site, '^&        ^'
+        )
+
+        obs = lt.observation_set.get()
+        self.assertEqual(
+            obs.last_updated,
+            timezone.make_aware(datetime.datetime(
+                2015, 7, 18, 4, 15, 10
+            ))
+        )
+        self.assertEqual(
+            obs.observation_datetime,
+            timezone.make_aware(datetime.datetime(
+                2015, 7, 19, 4, 15, 10
+            ))
+        )
+        self.assertEqual(
+            obs.observation_name,
+            "Aerobic bottle culture"
+        )
+        self.assertEqual(
+            obs.observation_number,
+            "12312"
+        )
+        self.assertEqual(
+            obs.reference_range,
+            "3.5 - 11"
+        )
+        self.assertEqual(
+            obs.units,
+            "g"
+        )
+        self.assertEqual(
+            obs.observation_value,
+            "123"
+        )
+
+        self.assertEqual(
+            lab_test_models.LabTest.objects.all().count(), 1
+        )
+
+    def test_updates_lab_test(self):
+        lab_test_models.LabTest.objects.create(**{
+            "patient": self.patient,
+            "clinical_info":  'testing',
+            "datetime_ordered": datetime.datetime(
+                2015, 7, 17, 4, 15, 10
+            ),
+            "lab_number": "11111",
+            "site": u'^&        ^',
+            "status": "Pending",
+            "test_code": "AN12",
+            "test_name": "Anti-CV2 (CRMP-5) antibodies",
+        })
+
+        lt, created = update_lab_tests.get_or_create_lab_test(
+            self.patient, self.api_dict
+        )
+
+        self.assertFalse(created)
+        self.assertEqual(lt.status, "Success")
+        # check the model did actually save
+        self.assertEqual(
+            lab_test_models.LabTest.objects.get().status,
+            "Success"
+        )
+
+
+
+
--- intrahospital_api/update_lab_tests.py
+++ intrahospital_api/update_lab_tests.py
@@ -1,4 +1,5 @@
 from elcid import models as emodels
+from plugins.labtests import models as lab_test_models
 from elcid.utils import timing
 from intrahospital_api import get_api
 
@@ -7,9 +8,9 @@ api = get_api()
 
 def update_tests(patient, lab_tests):
     """
-        takes in all lab tests, saves those
-        that need saving updates those that need
-        updating.
+    takes in all lab tests, saves those
+    that need saving updates those that need
+    updating.
     """
     for lab_test in lab_tests:
         lab_model = get_model_for_lab_test_type(patient, lab_test)
@@ -43,3 +44,26 @@ def get_model_for_lab_test_type(patient, lab_test):
         return by_test_type[0]
     else:
         return mod()
+
+def get_or_create_lab_test(patient, lab_test):
+    """"
+    Updates the plugins.labtest lab test if it exists
+    otherwise it creates it.
+    """
+    lts = patient.lab_tests.filter(
+        lab_number=lab_test["external_identifier"]
+    ).filter(
+        test_name=lab_test["test_name"]
+    )
+
+    lt = lts.first()
+
+    exists = bool(lt)
+
+    if not exists:
+        lt = lab_test_models.LabTest()
+
+    lt.update_from_api_dict(patient, lab_test)
+
+    return lt, not exists
+
