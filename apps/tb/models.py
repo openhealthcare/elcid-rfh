635e8f8371e6f475cde10906050ce7e8dcf64f89
fredkingham <fredkingham@gmail.com>
2019-04-01 18:02:57 +0100

    adds additional exposures to the travel model

--- apps/tb/models.py
+++ apps/tb/models.py
@@ -384,6 +384,7 @@ class Travel(models.EpisodeSubrecord):
     duration = fields.CharField(max_length=255, blank=True)
     reason_for_travel = ForeignKeyOrFreeText(models.Travel_reason)
     not_travelled = fields.NullBooleanField()
+    additional_exposures = fields.TextField(default="")
 
     class Meta:
         verbose_name = "Travel History"
