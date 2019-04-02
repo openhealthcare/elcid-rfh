ece7be04275c780be550de0fc3a97db4046b821a
fredkingham <fredkingham@gmail.com>
2019-04-02 09:53:30 +0100

    temporary markdown filter

--- apps/tb/static/js/tb/filters.js
+++ apps/tb/static/js/tb/filters.js
@@ -1,9 +1,22 @@
 filters.filter('bmi', function(){
-    "use strict";
-    return function(height, weight) {
-      if(height && weight){
-        var bmi = weight/(height * height);
-        return Math.round(bmi * 100) / 100
-      }
-    };
-  });
+  "use strict";
+  return function(height, weight) {
+    if(height && weight){
+      var bmi = weight/(height * height);
+      return Math.round(bmi * 100) / 100
+    }
+  };
+});
+
+filters.filter('markdown', function () {
+  return function(input){
+    if(!input){
+      return "";
+    }
+    var renderMarkdown = function(unrendered){
+      var converter = new Showdown.converter({extensions: [OpalDown]});
+      return converter.makeHtml(unrendered);
+    }
+    return renderMarkdown(input)
+  }
+});
