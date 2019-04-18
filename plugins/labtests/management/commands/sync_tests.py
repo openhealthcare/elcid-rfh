e1174806e1dc6f8c5a169ebee4f7f79e8a1a0908
fredkingham <fredkingham@gmail.com>
2019-04-18 20:24:20 +0100

    changing the lab test sync command to just look at lab tests from the lab test level rather than patient level and to sync if they do not already exist

--- plugins/labtests/management/commands/sync_tests.py
+++ plugins/labtests/management/commands/sync_tests.py
@@ -1,17 +1,24 @@
 """
 Syncs all old style tests with new style tests
 """
-
+from django.db import transaction
 from django.core.management.base import BaseCommand
-from plugins.labtests import models
-from opal.models import Patient
+from plugins.labtests import models as new_lab_models
+from lab import models as old_lab_models
 
 
 class Command(BaseCommand):
+    @transaction.atomic
     def handle(self, *args, **options):
-        for patient in Patient.objects.all():
-            for ut in patient.labtest_set.filter(
-                lab_test_type__istartswith="up"
-            ):
-                if not patient.lab_tests.exists():
-                    models.LabTest.create_from_old_test(ut)
+        lab_tests = old_lab_models.LabTest.objects.filter(
+            lab_test_type__istartswith="up"
+        )
+        processed_lab_numbers = new_lab_models.LabTest.objects.values_list(
+            "lab_number", flat=True
+        )
+        lab_tests = lab_tests.exclude(
+            external_identifier__in=processed_lab_numbers
+        )
+        for lab_test in lab_tests:
+            new_lab_models.LabTest.create_from_old_test(lab_test)
+
