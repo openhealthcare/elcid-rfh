085ae3a6f44e1f41f35f02b796c08d6d9b592c30
fredkingham <fredkingham@gmail.com>
2019-12-27 11:38:52 +0000

    Adds a management command to load in all lab tests since a defined date (27th of Dec 2019 00:00)

---
+++ intrahospital_api/management/commands/load_in_rows.py
@@ -0,0 +1,25 @@
+"""
+Randomise our admission dates over the last year.
+"""
+import datetime
+from django.core.management.base import BaseCommand
+from intrahospital_api.models import UpstreamLabTestRow
+from intrahospital_api.apis.prod_api import ProdApi
+from elcid.models import Demographics
+
+SINCE = datetime.datetime(2019, 12, 27)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        api = ProdApi()
+        all_rows = api.execute_query(
+            api.all_data_since_query,
+            params=dict(since=SINCE)
+        )
+        for row in all_rows:
+            if Demographics.objects.filter(
+                hospital_number=row["Patient_Number"]
+            ).exists():
+                UpstreamLabTestRow.create(row)
