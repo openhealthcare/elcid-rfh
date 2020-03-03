58889bc904bbc92106d45ae27f4f91d553cb6c9f
fredkingham <fredkingham@gmail.com>
2020-03-03 10:09:11 +0000

    bump the elcid version from 0.27 to 0.28

--- elcid/settings.py
+++ elcid/settings.py
@@ -334,7 +334,7 @@ else:
     EMAIL_HOST = 'localhost'
 
 
-VERSION_NUMBER = '0.27'
+VERSION_NUMBER = '0.28'
 
 #TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #TEST_RUNNER = 'django_test_coverage.runner.CoverageTestSuiteRunner'
