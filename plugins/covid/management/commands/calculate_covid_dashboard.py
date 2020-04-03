6e0175b44b6f04286da1de15d42ea0e89f3e935b
David Miller <david@deadpansincerity.com>
2020-04-03 10:20:37 +0100

    Don't print noise to logs in management comman

--- plugins/covid/management/commands/calculate_covid_dashboard.py
+++ plugins/covid/management/commands/calculate_covid_dashboard.py
@@ -15,17 +15,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         CovidDashboard.objects.all().delete()
-        print('Deleted Dashboard objects')
 
         tested = Patient.objects.filter(
             lab_tests__test_name=CORONAVIRUS_TEST_NAME).distinct()
-        print('Found Tested Patients')
 
         positive = tested.filter(
             lab_tests__observation__observation_value__in=POSITIVE_TEST_VALUES
         )
         positive_ids = [p.id for p in positive]
-        print('Found Positive Patients')
 
         negative = tested.filter(
             lab_tests__observation__observation_value__in=NEGATIVE_TEST_VALUES
