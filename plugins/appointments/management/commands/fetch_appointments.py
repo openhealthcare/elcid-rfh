6b561039b4da393dc15528f27af46db87f7598d8
David Miller <david@deadpansincerity.com>
2020-04-19 09:08:51 +0100

    Iterate through patients not demographics first

--- plugins/appointments/management/commands/fetch_appointments.py
+++ plugins/appointments/management/commands/fetch_appointments.py
@@ -4,8 +4,7 @@ Management command to fetch all appointments for our patients
 import traceback
 
 from django.core.management import BaseCommand
-
-from elcid.models import Demographics
+from opal.models import Patient
 
 from plugins.appointments import loader, logger
 
@@ -13,11 +12,10 @@ from plugins.appointments import loader, logger
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        for demographic in Demographics.objects.all():
+        for patient in Patient.objects.all():
             try:
-                patient = demographic.patient
                 loader.load_appointments(patient)
             except:
-                msg = 'Exception loading ICU Handover Patients \n {}'
+                msg = 'Exception loading Appointments \n {}'
                 logger.error(msg.format(traceback.format_exc()))
         return
