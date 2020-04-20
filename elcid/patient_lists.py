ac9f9903d01eb4adc44dbef8a18a402a3faaa9b8
David Miller <david@deadpansincerity.com>
2020-04-20 08:36:46 +0100

    Make antifungal lists superuser only

--- elcid/patient_lists.py
+++ elcid/patient_lists.py
@@ -63,6 +63,11 @@ class Antifungal(RfhPatientList, TaggedPatientList):
     template_name = 'episode_list.html'
     schema = []
 
+    @classmethod
+    def visible_to(klass, user):
+        return user.is_superuser
+
+
 
 class RnohWardround(RfhPatientList, TaggedPatientList):
     display_name = 'RNOH Ward Round'
@@ -255,6 +260,11 @@ class ChronicAntifungal(RfhPatientList, PatientList):
                 old_episodes = sorted(episode_ids, reverse=True)[1:]
                 to_remove = to_remove.union(old_episodes)
         return episodes.exclude(id__in=to_remove)
+
+    @classmethod
+    def visible_to(klass, user):
+        return user.is_superuser
+
 
 
 class OrganismPatientlist(AbstractBase):
