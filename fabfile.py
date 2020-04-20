8d970a861006dda0db36eef0fecd83cecf202dc2
David Miller <david@deadpansincerity.com>
2020-04-20 18:47:05 +0100

    Add cron job for appointment load. closes #912

--- fabfile.py
+++ fabfile.py
@@ -549,6 +549,27 @@ def write_cron_icu_load(new_env):
     ))
 
 
+def write_cron_appointment_load(new_env):
+    """
+    Creates a cron job that runs the appointment loader
+    """
+    print("Writing cron {}_appointment_load".format(PROJECT_NAME))
+    template = jinja_env.get_template(
+        'etc/conf_templates/cron_appointment_load.jinja2'
+    )
+    fabfile = os.path.abspath(__file__).rstrip("c")  # pycs won't cut it
+    output = template.render(
+        fabric_file=fabfile,
+        virtualenv=new_env.virtual_env_path,
+        unix_user=UNIX_USER,
+        project_dir=new_env.project_directory
+    )
+    cron_file = "/etc/cron.d/{0}_appointment_load".format(PROJECT_NAME)
+    local("echo '{0}' | sudo tee {1}".format(
+        output, cron_file
+    ))
+
+
 def write_cron_disk_check(new_env):
     """
     Creates a cron job that checks our disk space
@@ -793,6 +814,7 @@ def _deploy(new_branch, backup_name=None, remove_existing=False):
     write_cron_lab_pre_load(new_env)
     write_cron_sync_demographics(new_env)
     write_cron_icu_load(new_env)
+    write_cron_appointment_load(new_env)
     write_cron_disk_check(new_env)
     write_cron_calculate_dashboard(new_env)
 
