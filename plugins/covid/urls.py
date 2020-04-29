9e55923602fe8542fa976ef1bc2b97c3cdd324e1
David Miller <david@deadpansincerity.com>
2020-04-29 14:54:40 +0100

    Add a download button for superusers to extract the Covid Cohort

--- plugins/covid/urls.py
+++ plugins/covid/urls.py
@@ -10,5 +10,10 @@ urlpatterns = [
         'templates/covid/dashboard.html',
         views.CovidDashboardView.as_view(),
         name='covid_dashboard'
-    )
+    ),
+    url(
+        'covid/cohort-download/',
+        views.CovidCohortDownloadView.as_view(),
+        name='covid_download'
+    ),
 ]
