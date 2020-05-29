dd1a2d24d1465c1cb4b58b162b46c8c8213a7ffb
David Miller <david@deadpansincerity.com>
2020-05-29 12:19:07 +0100

    Pre-load covid tests for all test names in covid.lab

--- plugins/covid/management/commands/pre_load_covid19.py
+++ plugins/covid/management/commands/pre_load_covid19.py
@@ -3,35 +3,15 @@ Management command to pre-load Covid-19 patients
 """
 from django.core.management.base import BaseCommand
 
-from elcid.models import Demographics
-from elcid.episode_categories import InfectionService
-from intrahospital_api.apis.prod_api import ProdApi as ProdAPI
-from intrahospital_api.loader import create_rfh_patient_from_hospital_number
+from plugins.covid import logger, loader
 
-
-Q_GET_COVID_IDS = """
-SELECT DISTINCT Patient_Number FROM tQuest.Pathology_Result_view
-WHERE OBR_exam_code_Text = '2019 NOVEL CORONAVIRUS'
-"""
 
 class Command(BaseCommand):
     def handle(self, *a, **k):
-        api = ProdAPI()
-
-        results = api.execute_trust_query(Q_GET_COVID_IDS)
-
-        for result in results:
-
-            mrn = result['Patient_Number']
-
-            if Demographics.objects.filter(hospital_number=mrn).exists():
-                continue
-
-            create_rfh_patient_from_hospital_number(mrn, InfectionService)
-
-            self.stdout.write(self.style.SUCCESS(
-                'Created patient for {}'.format(mrn)
-            ))
-
+        try:
+            loader.pre_load_covid_patients()
+        except:
+            msg = 'Exception pre-loading covid patients \n {}'
+            logger.error(msg.format(traceback.format_exc()))
 
         return
