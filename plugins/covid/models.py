5c449b81efb9aef98fdcbc6452d3bdfa0bc43cdc
fredkingham <fredkingham@gmail.com>
2020-08-05 16:28:58 +0100

    make sure that we don't save empty strings or None for for interest or depressed. Make sure the model now handles the fact that the result stored has been trimmed

--- plugins/covid/models.py
+++ plugins/covid/models.py
@@ -574,7 +574,7 @@ class CovidFollowUpCall(EpisodeSubrecord):
     def phq_score(self):
         if self.interest is None or self.depressed is None:
             return None
-        return int(self.interest[1:2]) + int(self.depressed[1:2])
+        return int(self.interest) + int(self.depressed)
 
     def tsq_score(self):
         return len([i for i in range(1, 11) if getattr(self, 'tsq{}'.format(i))])
