766467692dcbf8d7e243f80bdd8932258e506b0b
David Miller <david@deadpansincerity.com>
2020-08-26 16:36:44 +0100

    Make icu lists auto

--- plugins/icu/patient_lists.py
+++ plugins/icu/patient_lists.py
@@ -27,3 +27,43 @@ class AutoICUSouthList(RfhPatientList, PatientList):
     @classmethod
     def visible_to(klass, user):
         return user.is_superuser
+
+
+class AutoICUEastList(RfhPatientList, PatientList):
+    is_read_only  = True
+    schema        = []
+    template_name = 'episode_list.html'
+    display_name  = 'Auto ICU East'
+    order         = 30
+
+    @property
+    def queryset(self):
+        patients = Patient.objects.filter(icuhandoverlocation__ward='East')
+        return Episode.objects.filter(
+            patient__in=patients,
+            category_name=InfectionService.display_name
+        ).distinct()
+
+    @classmethod
+    def visible_to(klass, user):
+        return user.is_superuser
+
+
+class AutoICUWestList(RfhPatientList, PatientList):
+    is_read_only  = True
+    schema        = []
+    template_name = 'episode_list.html'
+    display_name  = 'Auto ICU West'
+    order         = 30
+
+    @property
+    def queryset(self):
+        patients = Patient.objects.filter(icuhandoverlocation__ward='West')
+        return Episode.objects.filter(
+            patient__in=patients,
+            category_name=InfectionService.display_name
+        ).distinct()
+
+    @classmethod
+    def visible_to(klass, user):
+        return user.is_superuser
