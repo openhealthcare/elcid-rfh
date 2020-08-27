64c0281281ad861caa04c4eb0ab8eb18753e9272
David Miller <david@deadpansincerity.com>
2020-08-27 17:53:31 +0100

    set version number to 54

--- elcid/settings.py
+++ elcid/settings.py
@@ -374,7 +374,7 @@ else:
     EMAIL_HOST = 'localhost'
 
 
-VERSION_NUMBER = '0.53'
+VERSION_NUMBER = '0.54'
 
 #TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #TEST_RUNNER = 'django_test_coverage.runner.CoverageTestSuiteRunner'
