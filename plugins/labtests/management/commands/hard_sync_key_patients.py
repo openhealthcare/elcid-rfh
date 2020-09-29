62aa65991e255cab54e3771aa08c4f2c79dff297
David Miller <david@deadpansincerity.com>
2020-09-29 12:19:08 +0100

    Add recent views, don't re-sync demographics.

--- plugins/labtests/management/commands/hard_sync_key_patients.py
+++ plugins/labtests/management/commands/hard_sync_key_patients.py
@@ -1,21 +1,22 @@
 """
-Management command that checks that lab tests have been synced for the auto
-add ITU lists and bacteraemia.
-
-It resyncs those patients and sends an email with the result
+Management command that forces a hard sync of lab tests for key patients.
 """
+import datetime
+import time
 from collections import OrderedDict
+
 from django.conf import settings
 from django.core.mail import send_mail
 from django.core.management.base import BaseCommand
-from plugins.icu.patient_lists import AutoICUSouthList, AutoICUEastList, AutoICUWestList
-from django.utils.html import strip_tags
 from django.template.loader import render_to_string
-from plugins.labtests.models import Observation
-from opal.models import Patient
+from django.utils.html import strip_tags
+from opal.models import Patient, PatientRecordAccess
+
 from elcid.patient_lists import Bacteraemia
 from elcid.utils import timing
-from intrahospital_api import loader
+from intrahospital_api import loader, update_lab_tests
+from plugins.icu.patient_lists import AutoICUSouthList, AutoICUEastList, AutoICUWestList
+from plugins.labtests.models import Observation
 from plugins.labtests import logger
 
 
@@ -24,12 +25,19 @@ def get_patients():
     Returns a list of patients to have their lab tests checked
     """
     patient_ids = set()
-    patient_lists = [AutoICUSouthList, AutoICUEastList, AutoICUWestList, Bacteraemia]
+    patient_lists = [AutoICUSouthList, AutoICUEastList, AutoICUWestList, AutoICU2List, Bacteraemia]
+
     for patient_list in patient_lists:
         qs = patient_list().get_queryset()
         qs = qs.select_related('patient')
         for episode in qs:
             patient_ids.add(episode.patient_id)
+
+    recent = PatientRecordAccess.objects.filter(
+        created__gte=datetime.datetime.now()-datetime.timedelta(days=21))
+    for access in recent:
+        patient_ids.add(access.patient_id)
+
     return Patient.objects.filter(id__in=patient_ids)
 
 
@@ -39,10 +47,13 @@ def get_observations_count(patients):
 
 def sync_patients(patients):
     for patient in patients:
-        loader.sync_patient(patient)
+        results = loader.api.results_for_hospital_number(
+            patient.demographics().hospital_number
+        )
+        update_lab_tests.upd_tests(patient, results)
 
 
-def create_email(patients, before_observation_count, after_observation_count):
+def create_email(patients, before_observation_count, after_observation_count, time_taken):
     template_name = "email/table_email.html"
     title = "{} ICU & Bacteraemia lab test counts".format(
         settings.OPAL_BRAND_NAME
@@ -54,6 +65,7 @@ def create_email(patients, before_observation_count, after_observation_count):
         ("Patient count", len(patients),),
         ("Before patient sync count", before_observation_count,),
         ("After patient sync count", after_observation_count,),
+        ("Seconds", time_taken)
     ))
 
     send_email(title, template_name, {"table_context": table_context, "title": title})
@@ -78,8 +90,11 @@ def send_email(title, template_name, context):
 class Command(BaseCommand):
     @timing
     def handle(self, *a, **k):
+        t1 = time.time()
         patients = get_patients()
         before_observation_count = get_observations_count(patients)
         sync_patients(patients)
         after_observation_count = get_observations_count(patients)
-        create_email(patients, before_observation_count, after_observation_count)
+        t2 = time.time()
+
+        create_email(patients, before_observation_count, after_observation_count, int(t2-t1))
