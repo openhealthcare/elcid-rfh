f56c652a5609a803c0a050fff3712155f5db05dd
David Miller <david@deadpansincerity.com>
2020-10-20 10:29:40 +0100

    Management command to store backup size as a fact

---
+++ elcid/management/commands/get_backup_size.py
@@ -0,0 +1,30 @@
+"""
+Management command to track the size of our backups.
+Run by cron
+"""
+import datetime
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from plugins.monitoring.models import Fact
+
+
+class Command(BaseCommand):
+    def handle(self, *a, **k):
+        fmt = '/usr/lib/ohc/var/back.{}.elcidrfh_v{}.sql'
+        size_bytes = os.path.getsize(
+            fmt.format(
+                datetime.date.today().strftime('%d.%m.%Y'),
+                settings.VERSION_NUMBER.replace('.', ''))
+        )
+
+        mb = int(size_bytes/1073741824)
+
+        Fact(
+            when=timezone.make_aware(datetime.datetime.now()),
+            label="Backup size (MB)",
+            value_int=mb
+        ).save()
