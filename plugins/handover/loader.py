3d2fde673d9ccf66e1011359a62c34bf8d5645e1
David Miller <david@deadpansincerity.com>
2020-12-15 13:46:11 +0000

    Sync AMT trusting the discharged Flag

--- plugins/handover/loader.py
+++ plugins/handover/loader.py
@@ -66,3 +66,86 @@ def load_amt_handover():
         amt_status = patient.patientamthandoverstatus_set.get()
         amt_status.has_handover = True
         amt_status.save()
+
+
+
+Q_GET_CURRENT = """
+SELECT *
+FROM
+HandoverDB.View_Live_Acute_Medical_Team
+WHERE
+Discharged = 'N'
+"""
+
+Q_GET_HANDOVER_BY_ID = """
+SELECT *
+FROM
+HandoverDB.View_Live_Acute_Medical_Team
+WHERE
+id = @id
+"""
+
+
+
+def create_handover_from_upstream(data):
+    """
+    Given an upstream data dictionary returned by the API,
+    create a new AMTHandover record.
+    """
+    handover = AMTHandover()
+
+    handover.patient = Patient.objects.filter(
+        demographics__hospital_number=data['MRN']).first()
+
+    for k, v in result.items():
+        setattr(
+            handover,
+            AMTHandover.UPSTREAM_FIELDS_TO_MODEL_FIELDS[k],
+            v
+        )
+
+    handover.save()
+    return handover
+
+
+@transaction.atomic
+def sync_amt_handover():
+    """
+    The sync includes two operations:
+
+    - Replace our local copy of the AMT handover for current patients
+    - Update any that we think are current that have been discharged upstream
+    """
+    current_patients = api.execute_hospital_query(Q_GET_CURRENT)
+
+    current_ids = [h['id'] for h in current_patients]
+
+    for handover in current_patients:
+
+        mrn = handover['MRN']
+
+        if AMTHandover.objects.filter(sqlserver_id=handover['id']).exists():
+            AMTHandover.objects.get(sqlserver_id=handover['id']).delete()
+
+        if not Demographics.objects.filter(hospital_number=mrn).exists():
+            create_rfh_patient_from_hospital_number(mrn, InfectionService)
+
+            logger.info('Created patient for {}'.format(mrn))
+
+        create_handover_from_upstream(result)
+
+        amt_status = patient.patientamthandoverstatus_set.get()
+        if not amt_statush.has_handover:
+            amt_status.has_handover = True
+            amt_status.save()
+
+    discharged = AMTHandover.objects.exclude(
+        sqlserver_id__in=current_ids
+    ).filter(discharged='N')
+
+    for handover in discharged:
+        upstream = api.execute_hospital_query(
+            Q_GET_HANDOVER_BY_ID, id=handover.sqlserver_id)[0]
+
+        handover.delete()
+        create_handover_from_upstream(upstream)
