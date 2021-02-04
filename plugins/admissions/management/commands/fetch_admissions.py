b126e096f127611da4cfe3774571cbbe7f9e0007
David Miller <david@deadpansincerity.com>
2021-02-04 12:03:06 +0000

    Update cron job entrypoint for encounters'

--- plugins/admissions/management/commands/fetch_admissions.py
+++ plugins/admissions/management/commands/fetch_admissions.py
@@ -12,17 +12,9 @@ from plugins.admissions import loader, logger
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        failure_count = 0
-        for patient in Patient.objects.all():
-            try:
-                loader.load_encounters(patient)
-            except Exception:
-                failure_count += 1
-
-        if failure_count:
-            msg = "Failed to load admissions for {} patients \n".format(
-                failure_count
-            )
-            msg += "Last exception \n{}".format(traceback.format_exc())
+        try:
+            loader.load_recent_encounters()
+        except Exception:
+            msg += "Loading encounters:  \n{}".format(traceback.format_exc())
             logger.error(msg)
         return
