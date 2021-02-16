490e5807cea8949d25fb6f29e5bcf5c1a0beb52c
David Miller <david@deadpansincerity.com>
2021-02-16 10:26:34 +0000

    Auto create covid user

---
+++ plugins/covid/management/commands/create_covid_user.py
@@ -0,0 +1,67 @@
+"""
+Management Command to create a covid user
+"""
+import random
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from django.core.mail import send_mail
+from opal.models import UserProfile, Role
+
+
+def pw_please():
+    numbers = ['123', '456', '789']
+    nouns = ['Drums', 'Apples', 'Rabbits']
+    symbols = ['?', '!', ';']
+
+    return random.choice(numbers) + random.choice(nouns) + random.choice(symbols)
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('email')
+
+    def handle(self, *a, **k):
+
+        email = kwargs['email']
+        username, _ = email.split('@')
+
+        user = User(username=username)
+
+        password = pw_please()
+
+        user.set_password(password)
+
+        user.first_name = username
+        user.is_staff = True
+        user.save()
+
+        profile, _ = UserProfile.objects.get_or_create(user=user)
+        profile.force_password_change = True
+        profile.save()
+
+        for role_name in ['view_lab_tests_in_list', 'view_lab_tests_in_detail', 'view_lab_test_trends', 'covid19']:
+            profile.roles.add(Role.objects.get(name=role_name))
+
+
+        title = 'Your new elCID account'
+        plain_message = """An account has been created for you on the Elcid database at the Royal Free.
+
+You can access the system at the hospital or via VPN at http://elcid-rfh/
+
+Your username is {username}
+
+Your password has been set to {password}
+
+You will be asked to change it the first time you log in.
+
+Feel free to contact support@openhealthcare.org.uk if you encounter any issues.""".format(
+    username=username,
+    password=password)
+
+        send_mail(
+            title,
+            plain_message,
+            settings.DEFAULT_FROM_EMAIL,
+            email
+        )
