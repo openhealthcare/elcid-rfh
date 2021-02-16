cacb0be96cbbb999e3b67bdb47e50ec7f1df7084
David Miller <david@deadpansincerity.com>
2021-02-16 10:30:35 +0000

    Fixup: Import settings

--- plugins/covid/management/commands/create_covid_user.py
+++ plugins/covid/management/commands/create_covid_user.py
@@ -3,6 +3,7 @@ Management Command to create a covid user
 """
 import random
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
