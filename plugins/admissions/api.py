de492492a0dc5c40dfa7182f4c0c83cd52c3fd87
David Miller <david@deadpansincerity.com>
2021-05-10 10:37:14 +0100

    Message about admission exclusion

--- plugins/admissions/api.py
+++ plugins/admissions/api.py
@@ -7,6 +7,12 @@ from opal.models import Patient
 from opal.core.views import json_response
 
 EXCLUDE_ADMISSIONS = [
+    # We previously excluded these based on eyeballing data for admissions before
+    # we had learned to update them in place adequately.
+    # These message types are under active review, and the existing list definitely
+    # excluded current active admissions e.g. patients in the building where
+    # we had valid location data for them.
+
     # 'A14', # Pending admit
     # 'A35', # Merge account num
     # 'S12', # New Appt
