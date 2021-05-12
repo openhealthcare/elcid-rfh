c11279a2f291a1a370c206cef2943f4f6b30bc95
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-05-12 13:03:41 +0100

    Speed up the batch job by bulk creating observations for each patient

--- plugins/labtests/models.py
+++ plugins/labtests/models.py
@@ -35,7 +35,7 @@ class LabTest(models.Model):
     class Meta:
         ordering = ['-datetime_ordered']
 
-    def update_from_api_dict(self, patient, data):
+    def create_from_api_dict(self, patient, data):
         """
             This is the updateFromDict of the the UpstreamLabTest
 
@@ -71,15 +71,12 @@ class LabTest(models.Model):
         self.site = data["site"]
         self.test_name = data["test_name"]
         self.save()
+        observations = []
         for obs_dict in data["observations"]:
-            obs = self.observation_set.filter(
-                observation_number=obs_dict["observation_number"]
-            )
-            if obs.exists():
-                obs.delete()
-
-            obs = self.observation_set.create()
-            obs.create(obs_dict)
+            observation =  Observation.translate_to_object(obs_dict)
+            observation.test = self
+            observations.append(observation)
+        Observation.objects.bulk_create(observations)
 
     @classmethod
     def get_relevant_tests(self, patient):
@@ -181,12 +178,14 @@ class Observation(models.Model):
             "max": self.to_float(max_val)
         }
 
-    def create(self, observation_dict):
-        self.last_updated = serialization.deserialize_datetime(
+    @classmethod
+    def translate_to_object(cls, observation_dict):
+        obs = cls()
+        obs.last_updated = serialization.deserialize_datetime(
             observation_dict["last_updated"]
         )
         if observation_dict["observation_datetime"]:
-            self.observation_datetime = serialization.deserialize_datetime(
+            obs.observation_datetime = serialization.deserialize_datetime(
                 observation_dict["observation_datetime"]
             )
         fields = [
@@ -197,6 +196,10 @@ class Observation(models.Model):
             "units"
         ]
         for f in fields:
-            setattr(self, f, observation_dict.get(f))
-        self.save()
-        return self
+            setattr(obs, f, observation_dict.get(f))
+        return obs
+
+    def create(self, observation_dict):
+        obs = self.__class__.translate_to_object(observation_dict)
+        obs.save()
+        return obs
