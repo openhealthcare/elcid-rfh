9cc372f056b4dcbd1d8c973f8efd710e1052840f
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-05-23 20:54:13 +0100

    Adds thorasic follow up to the tb appointments list

--- plugins/tb/constants.py
+++ plugins/tb/constants.py
@@ -6,7 +6,8 @@ TB_APPOINTMENT_CODES = [
     "Thoracic TB F/Up",
     "Thoracic TB New",
     "Thoracic TB Nurse F/Up",
-    "Thoracic TB Nurse New"
+    "Thoracic TB Nurse New",
+    "Thoracic TB Telephone F/Up",
 ]
 
 TB_APPOINTMENT_REFRESH_TIME_FACT = "TB Appointment Refresh Seconds"
