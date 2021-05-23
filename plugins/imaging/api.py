ca965bbdfee6aab3b98303f222ac30c124ffd729
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-05-23 14:13:24 +0100

    Allow filtering by result type in the imaging api

--- plugins/imaging/api.py
+++ plugins/imaging/api.py
@@ -13,4 +13,7 @@ class ImagingViewSet(LoginRequiredViewset):
     def retrieve(self, request, pk):
         patient = get_object_or_404(Patient.objects.all(), pk=pk)
         imaging = patient.imaging.all().order_by('-date_reported')
+        result_code = request.GET.get("result_code")
+        if result_code:
+            imaging = imaging.filter(result_code=result_code)
         return json_response([r.to_dict() for r in imaging])
