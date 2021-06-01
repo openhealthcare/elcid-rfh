0d084491e9c62dd9d8e671ea4c80f5e60a944235
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-06-01 17:41:56 +0100

    refactor the upstream template context processor so that it does not use a private function

--- plugins/upstream_lists/context_processors.py
+++ plugins/upstream_lists/context_processors.py
@@ -1,19 +1,21 @@
 """
 Context Processors for Upstream Lists
 """
+from plugins.icu.models import ICUHandoverLocation
 from plugins.icu.models import current_icu_patients
 from django.utils.functional import SimpleLazyObject
 
 
+def get_icu_wards():
+    wards = ICUHandoverLocation.objects.filter(
+        patient__in=current_icu_patients()
+    ).values_list('ward', flat=True).distinct()
+    return [
+        (f'#/list/upstream/{ward}', f'Auto ICU {ward}') for ward in wards
+    ]
+
+
 def upstream_lists(request):
-    def get_icu_wards():
-        from plugins.icu.models import ICUHandoverLocation
-        wards = ICUHandoverLocation.objects.filter(
-            patient__in=current_icu_patients()
-        ).values_list('ward', flat=True).distinct()
-        return [
-            (f'#/list/upstream/{ward}', f'Auto ICU {ward}') for ward in wards
-        ]
     # Wrap it in a simple lazy object so that if the upstream_lists
     # are not referenced we don't do the calculation
     return {'upstream_lists': SimpleLazyObject(get_icu_wards)}
