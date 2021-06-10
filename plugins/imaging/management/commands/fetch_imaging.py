aa369df15872dbbdc12976c663a42939509b1ca0
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-06-10 13:26:05 +0100

    Change the management command to load for the last 48 hours

--- plugins/imaging/management/commands/fetch_imaging.py
+++ plugins/imaging/management/commands/fetch_imaging.py
@@ -2,27 +2,20 @@
 Management command to fetch imaging for all our patients
 """
 import traceback
-
+import datetime
+from django.utils import timezone
 from django.core.management import BaseCommand
-from opal.models import Patient
 
 from plugins.imaging import loader, logger
 
 
 class Command(BaseCommand):
-
     def handle(self, *args, **kwargs):
-        failure_count = 0
-        for patient in Patient.objects.all():
-            try:
-                loader.load_imaging(patient)
-            except Exception:
-                failure_count += 1
-
-        if failure_count:
-            msg = "Failed to load imaging for {} patients \n".format(
-                failure_count
-            )
+        two_days_ago = timezone.now() - datetime.timedelta(2)
+        try:
+            loader.load_imaging_since(two_days_ago)
+        except Exception:
+            msg = "Failed to load imaging"
             msg += "Last exception \n{}".format(traceback.format_exc())
             logger.error(msg)
         return
