02c8972f6c9ee12953e7b897fd86b8d0c672823d
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-06-17 08:16:27 +0100

    write facts about the number updated and the patient information count. Updates the demographics sync cron job to run the sync recent patient information instead

--- intrahospital_api/management/commands/sync_demographics.py
+++ intrahospital_api/management/commands/sync_demographics.py
@@ -1,13 +1,19 @@
 """
 A management command that is run by a cron job
-to run intrahospital_api.update_demographics.update_all_patient_information
+to run intrahospital_api.update_demographics.sync_recent_patient_information
 """
+import traceback
 from django.core.management.base import BaseCommand
-from intrahospital_api.update_demographics import update_all_patient_information
+from intrahospital_api.update_demographics import sync_recent_patient_information
+from intrahospital_api import logger
 
 
 class Command(BaseCommand):
     help = "Syncs all demographics with the Cerner upstream"
 
     def handle(self, *args, **options):
-        update_all_patient_information()
+        try:
+            sync_recent_patient_information()
+        except Exception:
+            msg = f'Exception writing patient information \n {traceback.format_exc()}'
+            logger.error(msg)
