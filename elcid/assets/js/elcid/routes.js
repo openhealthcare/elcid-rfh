0ed0177946689d7ff4a1900fb504e3fa08d9a5ec
David Miller <david@deadpansincerity.com>
2021-07-06 14:26:17 +0100

    View location history and contacts for admissions

--- elcid/assets/js/elcid/routes.js
+++ elcid/assets/js/elcid/routes.js
@@ -65,22 +65,39 @@ app.config(
              .when('/lab-sync-performance/', static_template_route('/templates/monitoring/lab_timings.html'))
              .when('/system-stats/',         static_template_route('/templates/monitoring/system_stats.html'))
 
-             .when('/tb/clinic-list/',  static_template_route('/templates/tb/clinic_list.html'))
-             .when('/tb/last-30-days/',  static_template_route('/templates/tb/last_30_days.html'))
-             .when('/tb/mdt-list/',  static_template_route('/templates/tb/mdt_list.html'))
+             .when('/tb/clinic-list/',       static_template_route('/templates/tb/clinic_list.html'))
+             .when('/tb/last-30-days/',      static_template_route('/templates/tb/last_30_days.html'))
+             .when('/tb/mdt-list/',          static_template_route('/templates/tb/mdt_list.html'))
 
              .when('/amt-covid/',            static_template_route('/templates/covid/amt_dashboard.html'))
              .when('/nursing-handover/',     static_template_route('/templates/nursing/dashboard.html'))
              .when('/beta/',                 static_template_route('/templates/elcid/beta.html'))
 
-             .when('/ipc/',                  static_template_route('/templates/ipc/home.html'))
-             .when('/ipc/wards/',            static_template_route('/templates/ipc/wards.html'))
-             .when('/ipc/ward/:ward_code/',  param_template_route('/templates/ipc/ward/', 'ward_code'))
-             .when('/ipc/siderooms/',        static_template_route('/templates/ipc/siderooms.html'))
+             .when('/admissions/transfer-history/:spell_number/',
+                   param_template_route(
+                       '/templates/admissions/transfer_history/', 'spell_number'))
+             .when('/admissions/slice-contacts/:slice_id/',
+                   param_template_route(
+                       '/templates/admissions/slice_contacts/', 'slice_id'
+                   ))
+             .when('/admissions/location-history/:location_code',
+                   param_template_route(
+                       '/templates/admissions/location-history/', 'location_code'
+                   ))
+
+             .when('/ipc/',                         static_template_route('/templates/ipc/home.html'))
+             .when('/ipc/wards/',                   static_template_route('/templates/ipc/wards.html'))
+             .when('/ipc/ward/:ward_code/',         param_template_route('/templates/ipc/ward/', 'ward_code'))
+             .when('/ipc/siderooms/',               static_template_route('/templates/ipc/siderooms.html'))
              .when('/ipc/recent-tests/:test_code/', param_template_route('/templates/ipc/recent-tests/', 'test_code'))
-             .when('/ipc/alert/:alert_code/', param_template_route('/templates/ipc/alert/', 'alert_code'))
-             .when('/patient-information-load-stats/', static_template_route('/templates/monitoring/patient_information_load_stats.html'))
-             .when('/imaging-load-stats/', static_template_route('/templates/monitoring/imaging_load_stats.html'))
+             .when('/ipc/alert/:alert_code/',       param_template_route('/templates/ipc/alert/', 'alert_code'))
+
+
+             .when('/patient-information-load-stats/', static_template_route(
+                 '/templates/monitoring/patient_information_load_stats.html'))
+             .when('/imaging-load-stats/',             static_template_route(
+                 '/templates/monitoring/imaging_load_stats.html'))
+
              .when('/nursing-handover/:ward_code/',      {
                  controller: 'WelcomeCtrl',
                  controllerAs: 'welcome',
