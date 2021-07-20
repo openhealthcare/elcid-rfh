60d9021095cc289cbd05e97e72f77bf9a56abe96
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-07-20 13:19:36 +0100

    Remove the querystring template tag as this is no longer required

--- elcid/templatetags/elcid_panels.py
+++ elcid/templatetags/elcid_panels.py
@@ -9,29 +9,3 @@ register = template.Library()
 @register.inclusion_tag('templatetags/wide_panel.html', takes_context=True)
 def wide_panel(context, *args, **kwargs):
     return record_panel(context, *args, **kwargs)
-
-
-@register.simple_tag(takes_context=True)
-def query_string(context, replace=True, **kwargs):
-    """
-    Updates the current querydict with the required field
-
-    By default it replaces the key
-
-    It ignores the cacheBust key
-    e.g.
-    if we were at /?hello=there&good_bye=old_friend
-    we could do
-    /?{% query_string good_bye='viajo_amigo' %}
-    and the template would render
-    /?hello=there&good_bye=viajo_amigo
-    """
-    query_dict = context["request"].GET.copy()
-    if "cache_bust" in query_dict:
-        query_dict.pop("cache_bust")
-    if replace:
-        for k in kwargs.keys():
-            if k in query_dict:
-                query_dict.pop(k)
-    query_dict.update(kwargs)
-    return query_dict.urlencode()
