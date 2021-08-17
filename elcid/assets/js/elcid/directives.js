2b20c4c0dfe0ad7a1d660e9067f18214feee0452
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-08-17 10:06:36 +0100

    Change the patient-api to reload if the get params change

--- elcid/assets/js/elcid/directives.js
+++ elcid/assets/js/elcid/directives.js
@@ -31,20 +31,24 @@ directives.directive('patientApi', function($http, $window){
         link: function(scope, element, attrs){
             var endpoint = attrs.endpoint;
             var target   = attrs.target;
-            var url = '/api/v0.1/' + endpoint + '/' + '' + scope.patient.id + '' + '/';
-            if(attrs.getParams){
-              var getParams = scope.$eval(attrs.getParams);
-              url += '?' + $.param(getParams);
-            }
-            $http({cache: true, url: url, method: 'GET'}).then(
+
+            var loadData = function(){
+              var url = '/api/v0.1/' + endpoint + '/' + '' + scope.patient.id + '' + '/';
+              if(attrs.getParams){
+                var getParams = scope.$eval(attrs.getParams);
+                url += '?' + $.param(getParams);
+              }
+              $http({cache: true, url: url, method: 'GET'}).then(
                 function(response){
                     scope[target] = response.data;
                 },
                 function(){
                     $window.alert('Error loading data from the server')
                 }
-            )
-
+              )
+            }
+            scope.$watch(attrs.getParams, loadData, true);
+            loadData();
         }
     }
 });
