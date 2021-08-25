0b921a4ee620dac052055854050e8d255a118b65
David Miller <david@deadpansincerity.com>
2021-08-25 12:34:44 +0100

    RNOH Ward names tweak

--- plugins/rnoh/constants.py
+++ plugins/rnoh/constants.py
@@ -2,19 +2,27 @@
 Constants for the RNOH plugin
 """
 GROUPED_WARD_NAMES = [
+    'Clinic',
     'LIRI',
     'DOG',
     'ITU/CHDU',
     'JBW',
-    'SCIC'
+    'SCIC',
+    'TCU',
+    'JUB',
+    'PPU',
+    'COX',
+    'New-Results',
+    'Reference-Lab',
+    'Jobs',
+
 ]
 
 INDIVIDUAL_WARD_NAMES = [
     'MDT-Spinal',
     'MDT-JRU',
+    'MDT-LRU',
+    'MDT-Upper-Limb',
     'OPAT',
-    'Reference-La',
-    'Jobs',
-    'New-Results',
     'Misc'
 ]
