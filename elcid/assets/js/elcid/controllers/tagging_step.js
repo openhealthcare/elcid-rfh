bd0fe7f0581d9f6432946938a2abc3b15c738061
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-10-01 16:51:04 +0100

    Use the new teams select in the add patient pathway

--- elcid/assets/js/elcid/controllers/tagging_step.js
+++ elcid/assets/js/elcid/controllers/tagging_step.js
@@ -1,6 +1,55 @@
 angular.module('opal.controllers').controller('TaggingStepCtrl',
-  function(scope, step, episode) {
+  function(scope, step, $http, Metadata) {
       "use strict";
-      scope.editing.tagging = {};
+
+      var INFECTION_SERVICE = "infection_service";
+      var RNOH = "rnoh"
+
+      scope.categorySlug = INFECTION_SERVICE
+
+      scope.init = function(){
+        scope.value = [];
+        scope.tagsList = [];
+        scope.editing.tagging = {};
+        var url = "/elcid/v0.1/tags_for_category/" + scope.categorySlug + '/';
+        $http({cache: true, url: url, method: 'GET'}).then(function(response){
+          Metadata.load().then(function(metadata){
+            _.each(scope.editing.tagging, function (value, key) {
+              if(value && key !== 'id' && !key.startsWith("_")){
+                scope.value.push(metadata.tags[key]);
+              }
+            });
+            _.each(response.data, function(tagSlug){
+              scope.tagsList.push(metadata.tags[tagSlug])
+            });
+            loading = false;
+          })
+        })
+      }
+
+      scope.onRemove = function($item, $modal){
+        scope.editing.tagging[$modal] = false;
+      }
+
+      scope.onSelect = function($item, $modal){
+        scope.editing.tagging[$modal] = true;
+      }
+
+      scope.$watch('editing.location.hospital', function(){
+        if(scope.editing.location.hospital === 'RNOH'){
+          if(scope.categorySlug !== RNOH){
+            scope.categorySlug = RNOH;
+            scope.init();
+          }
+        }
+        else{
+          if(scope.categorySlug !== INFECTION_SERVICE){
+            scope.categorySlug = INFECTION_SERVICE;
+            scope.init();
+          }
+        }
+      });
+
+      scope.init();
   }
 );
