17d1a4b33168a676f056e5007a56d90894c1abe0
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-10-01 13:28:53 +0100

    bumps the version to v0.92

--- elcid/settings.py
+++ elcid/settings.py
@@ -391,7 +391,7 @@ else:
     EMAIL_HOST = 'localhost'
 
 
-VERSION_NUMBER = '0.91'
+VERSION_NUMBER = '0.92'
 
 #TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #TEST_RUNNER = 'django_test_coverage.runner.CoverageTestSuiteRunner'
