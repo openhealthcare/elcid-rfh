78e278437ec3492bd50f05e063e3b5ca9b12a736
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-10-26 15:30:50 +0100

    Adds facts for the discharge summary loader

--- plugins/dischargesummary/management/commands/fetch_dischargesummaries.py
+++ plugins/dischargesummary/management/commands/fetch_dischargesummaries.py
@@ -2,14 +2,43 @@
 Management command to fetch discharge summaries for our patients
 """
 import traceback
-
+import datetime
+import time
 from django.core.management import BaseCommand
-from opal.models import Patient
-
-from plugins.dischargesummary import loader, logger
+from django.utils import timezone
+from plugins.dischargesummary import loader, logger, constants
+from plugins.dischargesummary.models import (
+    DischargeMedication, DischargeSummary
+)
+from plugins.monitoring.models import Fact
 
 
 class Command(BaseCommand):
 
     def handle(self, *a, **k):
-        loader.load_all_discharge_summaries()
+        three_days_ago = datetime.date.today() - datetime.timdelta(3)
+        time_start = time.time()
+        try:
+            loader.load_discharge_summaries_since(three_days_ago)
+            time_end = time.time()
+            now = timezone.now()
+            Fact.objects.create(
+                when=now,
+                label=constants.DISCHARGE_SUMMARY_LOAD_FACT,
+                value_int=(time_end-time_start)
+            )
+            Fact.objects.create(
+                when=now,
+                label=constants.TOTAL_DISCHARGE_SUMMARIES,
+                value_int=DischargeSummary.objects.all().count()
+            )
+            Fact.objects.create(
+                when=now,
+                label=constants.TOTAL_DISCHARGE_MEDICATIONS,
+                value_int=DischargeMedication.objects.all().count()
+            )
+        except Exception:
+            msg = "Failed to load discharge summaries"
+            msg += "Last exception \n{}".format(traceback.format_exc())
+            logger.error(msg)
+        return
