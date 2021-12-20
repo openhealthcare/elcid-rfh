1693f60de5f172ad3d78b032a7c7ceb8b4a83724
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-12-20 19:00:27 +0000

    Adds in the side rooms, which are actually isolated wards

--- plugins/ipc/api.py
+++ plugins/ipc/api.py
@@ -1,9 +1,12 @@
+import collections
 from rest_framework.decorators import action
-from rest_framework import status
+from rest_framework import status as http_status
 from django.utils.functional import cached_property
 from opal.core.views import json_response
-from plugins.admissions.models import BedStatus, IsolatedBed
 from opal.core.api import LoginRequiredViewset
+from plugins.admissions.models import BedStatus, IsolatedBed
+from plugins.admissions.constants import RFH_HOSPITAL_SITE_CDOE
+from plugins.ipc.episode_categories import IPCEpisode
 
 
 class BedStatusApi(LoginRequiredViewset):
@@ -36,19 +39,56 @@ class BedStatusApi(LoginRequiredViewset):
             result[field.name] = getattr(instance, field.name)
         result["in_isolation"] = self.in_isolation(instance)
         if instance.patient_id:
+            result["demographics"] = instance.patient.demographics_set.all()[0].to_dict(
+                self.request
+            )
             result["comments"] = instance.patient.ipcstatus_set.all()[0].comments
+            episodes = instance.patient.episode_set.all()
+            ipc_episodes = [i for i in episodes if i.category_name == IPCEpisode.display_name]
+            if ipc_episodes:
+                result['episode_id'] = ipc_episodes[0].episode_id
         return result
+
+    @action(detail=False, methods=["get"], url_path="isolation")
+    def isolation(self, request):
+        side_room_statuses = list(BedStatus.objects.filter(
+            room__startswith='SR'
+        ))
+        other_statuses = BedStatus.objects.filter(
+            hospital_site_code=RFH_HOSPITAL_SITE_CDOE,
+        ).exclude(
+            room__startswith='SR'
+        )
+        isolated_statuses = [i for i in other_statuses if self.in_isolation(i)]
+        statuses = BedStatus.objects.filter(
+            id__in=[i.id for i in side_room_statuses + isolated_statuses]
+        ).order_by(
+            'ward_name', 'bed'
+        ).prefetch_related(
+            'patient__episode_set',
+            'patient__ipcstatus_set',
+            'patient__demographics_set',
+        )
+        wards = collections.defaultdict(list)
+        for status in statuses:
+            wards[status.ward_name].append(self.serialize_instance(status))
+        return json_response({
+            "location_count": len(statuses),
+            "wards": wards
+        })
 
     @action(detail=False, methods=["get"], url_path="ward/(?P<ward_name>[^/.]+)")
     def ward_list(self, request, ward_name):
-        return json_response(
-            [
-                self.serialize_instance(i)
-                for i in BedStatus.objects.filter(ward_name=ward_name).prefetch_related(
-                    "patient__ipcstatus_set"
-                )
-            ]
+        statuses = BedStatus.objects.filter(ward_name=ward_name).order_by(
+            'ward_name', 'bed'
+        ).prefetch_related(
+            'patient__episode_set',
+            "patient__ipcstatus_set",
+            'patient__demographics_set',
         )
+        return json_response({
+            "patients": [self.serialize_instance(i) for i in statuses]
+        })
 
     @action(
         detail=True,
@@ -63,4 +103,4 @@ class BedStatusApi(LoginRequiredViewset):
             room=bed_status.room,
             bed=bed_status.bed,
         )
-        return json_response({}, status_code=status.HTTP_204_NO_CONTENT)
+        return json_response({}, status_code=http_status.HTTP_204_NO_CONTENT)
--- plugins/ipc/static/ipc/js/controllers/bed_status.js
+++ plugins/ipc/static/ipc/js/controllers/bed_status.js
@@ -2,8 +2,8 @@ angular.module('opal.controllers').controller('BedStatus', function(
 	$scope, $http, queryResponse
 ){
 	"use strict";
-	$scope.patients = queryResponse.data;
 
+	$scope.data = queryResponse.data;
 	$scope.isolate = function(bedStatus, inIsolation){
 		var args = {in_isolation: inIsolation}
 		$http.put('/api/v0.1/bed_status/' + bedStatus.id + '/isolate/', args).then(
