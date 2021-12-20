622c26680b42dfae13f5eec45a7c4ba6e00cb9d8
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-12-20 22:20:18 +0000

    Add in an isolated beds columns to the ward columns

--- plugins/ipc/plugin.py
+++ plugins/ipc/plugin.py
@@ -4,8 +4,17 @@ Plugin defnition for the handover plugin
 from opal.core import plugins
 
 from plugins.ipc.urls import urlpatterns
+from plugins.ipc import api
+
 
 class IPCPlugin(plugins.OpalPlugin):
     urls = urlpatterns
 
-    apis = []
+    apis = [
+        (api.IsolatedBedApi.basename, api.IsolatedBedApi,),
+    ]
+    javascripts = {
+        'opal.controllers': [
+            "ipc/js/controllers/isolationHelper.js"
+        ]
+    }
