b12dbb8a981ccd9e25683de096ea47ee377504c7
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-01-06 13:57:49 +0000

    Make the covid send upstream modal also include the patients name, dob and mrn

--- plugins/covid/static/js/covid/send_covid_to_epr.controller.js
+++ plugins/covid/static/js/covid/send_covid_to_epr.controller.js
@@ -1,19 +1,28 @@
 angular.module('opal.controllers').controller(
 	'SendCovidToEPR',
-	function($scope, $modalInstance, $http, $q, modelApiName, itemId, callBack){
-			$scope.send_upstream = function(){
-					var url = "/api/v0.1/epr_" + modelApiName + "/" + itemId + "/";
-					$http.put(url).then(
-							function(response){
-								$q.when(callBack()).then(function(){
-									$modalInstance.close();
-								});
-							}
-					)
-			}
+	function($scope, $modalInstance, $http, $q, modelApiName, itemId, callBack, first_name, surname, hospital_number, date_of_birth){
+		$scope._patient = {
+			demographics: [{
+				'first_name': first_name,
+				'surname': surname,
+				'date_of_birth': date_of_birth,
+				'hospital_number': hospital_number
+			}]
+		}
 
-			$scope.cancel = function(){
-					$modalInstance.close('cancel');
-			}
+		$scope.send_upstream = function(){
+				var url = "/api/v0.1/epr_" + modelApiName + "/" + itemId + "/";
+				$http.put(url).then(
+						function(response){
+							$q.when(callBack()).then(function(){
+								$modalInstance.close();
+							});
+						}
+				)
+		}
+
+		$scope.cancel = function(){
+				$modalInstance.close('cancel');
+		}
 	}
 );
