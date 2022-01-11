75c0b780f65a4438e12c007bf8262943317df1df
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-01-11 13:34:40 +0000

    Bumps the version number to v0.103

--- elcid/settings.py
+++ elcid/settings.py
@@ -392,7 +392,7 @@ else:
     EMAIL_HOST = 'localhost'
 
 
-VERSION_NUMBER = '0.102'
+VERSION_NUMBER = '0.103'
 
 #TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #TEST_RUNNER = 'django_test_coverage.runner.CoverageTestSuiteRunner'
