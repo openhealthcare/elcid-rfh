ba13fec6bfe2570d020fd62ddaf1c20a115d3f85
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-01-25 10:21:04 +0000

    Speed up the enounter load

--- plugins/admissions/loader.py
+++ plugins/admissions/loader.py
@@ -2,6 +2,7 @@
 Load admissions from upsteam
 """
 import datetime
+from collections import defaultdict
 
 from django.db import transaction
 from django.db.models import DateTimeField
@@ -12,7 +13,7 @@ from elcid.episode_categories import InfectionService
 from elcid.models import Demographics
 from intrahospital_api.apis.prod_api import ProdApi as ProdAPI
 
-from plugins.admissions.models import Encounter, TransferHistory, BedStatus
+from plugins.admissions.models import Encounter, PatientEncounterStatus, TransferHistory, BedStatus
 from plugins.admissions import logger
 
 
@@ -42,20 +43,15 @@ FROM INP.CURRENT_BED_STATUS
 WITH (NOLOCK)
 """
 
-def save_encounter(encounter, patient):
+
+def cast_to_encounter(encounter, patient):
     """
     Given a dictionary of ENCOUNTER data from the upstream database,
     and the PATIENT for whom it concerns, save it.
     """
-    our_encounter, created = Encounter.objects.get_or_create(
-        patient=patient, upstream_id=encounter['ID']
-    )
-
-    if not created:
-        logger.info('Updating existing encounter')
+    our_encounter = Encounter(patient=patient, upstream_id=encounter['ID'])
 
     for k, v in encounter.items():
-
         if v: # Ignore for empty / nullvalues
             # Empty is actually more complicated than pythonic truthiness.
             # Many admissions have the string '""' as the contents of room/bed
@@ -80,11 +76,36 @@ def save_encounter(encounter, patient):
                 our_encounter,
                 Encounter.UPSTREAM_FIELDS_TO_MODEL_FIELDS[k], v
             )
+    return our_encounter
 
-    our_encounter.save()
-    logger.info('Saved encounter {}'.format(our_encounter.pk))
 
-    return our_encounter, created
+def update_encounters_from_query_result(rows):
+    upstream_ids = [i['ID'] for i in rows]
+    existing_encounters = Encounter.objects.filter(upstream_id__in=upstream_ids)
+    existing_encounters_count = existing_encounters.count()
+    logger.info(f'{existing_encounters_count} existing encounters will be removed')
+    mrns = [i['PID_3_MRN'].strip() for i in rows if i['PID_3_MRN'].strip()]
+    patients = Patient.objects.filter(
+        demographics__hospital_number__in=mrns
+    ).prefetch_related('demographics_set')
+
+    mrn_to_patients = defaultdict(list)
+    for patient in patients:
+        mrn_to_patients[patient.demographics_set.all()[0].hospital_number].append(
+            patient
+        )
+
+    to_create = []
+    for row in rows:
+        for patient in mrn_to_patients[row['PID_3_MRN'].strip()]:
+            to_create.append(cast_to_encounter(row, patient))
+    logger.info(f'Creating {len(to_create)} encounters')
+    Encounter.objects.bulk_create(to_create, batch_size=500)
+    PatientEncounterStatus.objects.filter(
+        patient__in=patients
+    ).update(
+        has_encounters=True
+    )
 
 
 def load_encounters(patient):
@@ -94,21 +115,11 @@ def load_encounters(patient):
     api = ProdAPI()
 
     demographic     = patient.demographics()
-    encounter_count = patient.encounters.count()
-
     encounters = api.execute_hospital_query(
         Q_GET_ALL_PATIENT_ENCOUNTERS,
         params={'mrn': demographic.hospital_number}
     )
-    for encounter in encounters:
-        save_encounter(encounter, patient)
-
-    if encounter_count == 0:
-        if patient.encounters.count() > 0:
-            # We've stored the first encounter for this patient
-            status = patient.patientencounterstatus_set.get()
-            status.has_encounters = True
-            status.save()
+    update_encounters_from_query_result(encounters)
 
 
 def load_excounters_since(timestamp):
@@ -136,20 +147,16 @@ def load_excounters_since(timestamp):
         params={'timestamp': timestamp}
     )
     for encounter in encounters:
-        mrn = encounter['PID_3_MRN'].strip()
-
-        if mrn == '':
-            continue
-
-        if Demographics.objects.filter(hospital_number=mrn).exists():
-            patient = Patient.objects.filter(demographics__hospital_number=mrn).first()
-            save_encounter(encounter, patient)
-        else:
-            patient = create_rfh_patient_from_hospital_number(mrn, InfectionService)
-            save_encounter(encounter, patient)
-        patient.patientencounterstatus_set.update(
-            has_encounters=True
-        )
+        if encounter['PID_3_MRN']:
+            encounter['PID_3_MRN'] = encounter['PID_3_MRN'].strip()
+    mrns = list(set([i['PID_3_MRN'] for i in encounters if i['PID_3_MRN']]))
+    existing_hns = set(Demographics.objects.filter(
+        hospital_number__in=mrns).values_list('hospital_number', flat=True)
+    )
+    for mrn in mrns:
+        if mrn and mrn not in existing_hns:
+            create_rfh_patient_from_hospital_number(mrn, InfectionService)
+    update_encounters_from_query_result(encounters)
 
 
 def load_transfer_history():
