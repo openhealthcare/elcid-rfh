8ab22ee74bb5b9022b85580efa236c2a274d645e
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-01-26 16:32:15 +0000

    Adds in a front end epma detail

---
+++ plugins/epma/static/js/epma/controllers/epma_detail.js
@@ -0,0 +1,53 @@
+angular.module('opal.controllers').controller('EPMAView', function(
+	$scope, $http, $q, $window, ngProgressLite
+){
+	"use strict";
+	var vm = this;
+
+	vm.epmaOrders = []
+
+	// we display all fields coming from the api
+	// chunked into arrays of 2
+	vm.chunkedOrderFields = []
+
+	// all the fields on the detail object
+	vm.detailKeys = []
+
+	var url_base = '/api/v0.1/epma_med_order/';
+
+	vm.load = function(patient_id){
+			var url = url_base + patient_id + '/';
+			ngProgressLite.set(0);
+			ngProgressLite.start();
+
+			$http({cache: true, url: url, method: 'GET' }).then(
+					function(response){
+							vm.epmaOrders = response.data;
+							if(vm.epmaOrders.length){
+								vm.chunkedOrderFields = vm.getChunkedOrderFields(vm.epmaOrders[0]);
+								var hasDetail = _.findWhere(vm.epmaOrders, function(order){
+									return order.details.length
+								});
+								vm.detailKeys = _.keys(hasDetail.details[0])
+							}
+							ngProgressLite.done();
+					},
+					function(){
+							ngProgressLite.done();
+							$window.alert('EPMA data could not be loaded');
+					}
+			);
+
+	}
+
+
+
+	vm.getChunkedOrderFields = function(obj){
+		var keys = _.keys(obj);
+		keys = _.without(keys, 'details', 'id', 'created_in_elcid')
+		return _.chunk(keys, 2);
+	}
+
+	vm.load($scope.patient.id);
+
+});
