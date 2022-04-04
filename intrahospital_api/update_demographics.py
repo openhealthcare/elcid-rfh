e0362d2f761aa9ca881e57a8ceaef474e0dc4010
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-04-04 13:34:13 +0100

    Brings in the upstream demographics search functionality from the epr search

--- intrahospital_api/update_demographics.py
+++ intrahospital_api/update_demographics.py
@@ -412,3 +412,77 @@ def update_patient_information_since(last_updated):
     logger.info(f"patient information: patients found {number_of_patients_found}")
     logger.info(f"patient information: patients updated {len(new_master_files)}")
     return len(new_master_files)
+
+
+def find_patient_upstream(
+    number=None, date_of_birth=None, surname=None
+):
+    """
+    The upstream demographics used by the DemographicsSearch api
+    if its a number it searches by both hospital number and nhs
+    number
+    """
+    if number:
+        return demographics_search(
+            hospital_number=number,
+            nhs_number=number,
+            conjunction='OR'
+        )
+    return demographics_search(
+        date_of_birth=date_of_birth,
+        surname=surname
+    )
+
+@timing
+def demographics_search(
+    *args,
+    conjunction='AND',
+    hospital_number=None,
+    nhs_number=None,
+    surname=None,
+    date_of_birth=None
+):
+    """
+    Allows searching by named arguments
+    expects 'hospital_number', 'nhs_number', 'surname'
+    or 'date_of_birth'
+
+    hospital number and surname are searched
+    case insensitively.
+
+    By default its an 'AND' query, but conjunction
+    can be changed to 'OR'
+    """
+    if any([hospital_number, nhs_number, surname, date_of_birth]):
+        raise ValueError('Demographics search expects arguments')
+    query_args = []
+    params = {}
+    if hospital_number:
+        query_args.append("(UPPER(PATIENT_NUMBER) = UPPER(@hospital_number))")
+        params["hospital_number"] = hospital_number
+    if nhs_number:
+        query_args.append("(NHS_NUMBER = @nhs_number)")
+        params["nhs_number"] = nhs_number
+    if surname:
+        query_args.append("(UPPER(SURNAME) = UPPER(@surname))")
+        params["surname"] = surname
+    if date_of_birth:
+        query_args.append("(DOB = @date_of_birth)")
+        params["date_of_birth"] = date_of_birth
+    clauses = f" {conjunction } ".join(
+        query_args
+    )
+    query = """
+    SELECT PATIENT_NUMBER, NHS_NUMBER, SURNAME, FORENAME1, DOB
+    FROM VIEW_CRS_Patient_Masterfile WHERE """ + clauses
+    query_result = api.execute_hospital_query(query, params=params)
+    result = []
+    for row in query_result:
+        result.append({
+            "hospital_number": row["PATIENT_NUMBER"],
+            "nhs_number": row["NHS_NUMBER"],
+            "surname": row["SURNAME"],
+            "first_name": row["FORENAME1"],
+            "date_of_birth": row["DOB"]
+        })
+    return result
