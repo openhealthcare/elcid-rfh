a1bdac872c6949c0e6c5b75e2a05f37f2b6ac040
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-09-07 13:51:24 +0100

    Fixes the unit tests for the appointment loader
    Finishes the fixes for the appointment loader after the refactor in 25e6175b4a619bb5b384a3d09cf69036a6748df6

--- plugins/appointments/tests/test_loader.py
+++ plugins/appointments/tests/test_loader.py
@@ -15,15 +15,20 @@ from plugins.appointments import loader
 class LoadAppointmentTestCase(OpalTestCase):
     def setUp(self):
         p, e = self.new_patient_and_episode_please()
+        p.demographics_set.update(
+            hospital_number="2345"
+        )
         self.patient = p
 
     def test_load_apppointment(self):
         now = datetime.datetime.now()
         appointment_data = {
+            'vPatient_Number'           : '2345',
             'Appointment_ID'            : '1234',
             'Appointment_Start_Datetime': now,
             'Appointment_Status_Code'   : 'CONFIRMED',
             'insert_date'               : now,
+            'last_updated'              : None,
             'HL7_Message_ID'            : '4567',
         }
 
@@ -36,9 +41,15 @@ class LoadAppointmentTestCase(OpalTestCase):
         self.assertEqual('CONFIRMED', self.patient.appointments.get().status_code)
 
 
-    def test_load_apppointment_some_exist(self):
+    @mock.patch('plugins.appointments.loader.get_changed_appointment_fields')
+    def test_load_apppointment_some_exist(self, get_changed_appointment_fields):
         now = datetime.datetime.now()
         then = now - datetime.timedelta(days=2)
+
+        # get changed appointments is there for logging purposes only
+        # and requires a full dictionary of upstream appointment data
+        # so lets return an empty dictionary
+        get_changed_appointment_fields.return_value = {}
 
         Appointment.objects.create(
             patient=self.patient,
@@ -47,10 +58,13 @@ class LoadAppointmentTestCase(OpalTestCase):
         )
 
         appointment_data = {
+            'vPatient_NHS_Number'       : None,
+            'vPatient_Number'           : '2345',
             'Appointment_ID'            : '1234',
             'Appointment_Start_Datetime': now,
             'Appointment_Status_Code'   : 'CONFIRMED',
             'insert_date'               : now,
+            'last_updated'              : now,
             'HL7_Message_ID'            : '4567',
         }
 
