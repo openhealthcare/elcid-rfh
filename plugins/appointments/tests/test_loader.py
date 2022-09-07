9c3c9b4145e8cb593acbe338bb4ed3c756b84572
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-09-07 13:03:51 +0100

    Removes the NonDateIdenticalTestCase test case
    The non_date_identical no longer exists as of
    25e6175b4a619bb5b384a3d09cf69036a6748df6 so remove the related unit
    tests

--- plugins/appointments/tests/test_loader.py
+++ plugins/appointments/tests/test_loader.py
@@ -12,48 +12,6 @@ from plugins.appointments.models import Appointment
 from plugins.appointments import loader
 
 
-
-class NonDateIdenticalTestCase(OpalTestCase):
-
-    def test_identical(self):
-        dict_one = {
-            'appointment_id'  : '1234',
-            'start_datetime'  : '2019-01-01 12:00:00'
-        }
-        dict_two = {
-            'Appointment_ID'            : '1234',
-            'Appointment_Start_Datetime': '2019-01-01 12:00:00'
-        }
-        appointment = Appointment(**dict_one)
-        self.assertTrue(loader.non_date_identical(appointment, dict_two))
-
-
-    def test_dates_different(self):
-        dict_one = {
-            'appointment_id'  : '1234',
-            'start_datetime'  : '2019-01-01 12:00:00'
-        }
-        dict_two = {
-            'Appointment_ID'            : '1234',
-            'Appointment_Start_Datetime': '2009-02-01 12:00:00'
-        }
-        appointment = Appointment(**dict_one)
-        self.assertTrue(loader.non_date_identical(appointment, dict_two))
-
-
-    def test_non_dates_different(self):
-        dict_one = {
-            'appointment_id'  : '12',
-            'start_datetime'  : '2019-01-01 12:00:00'
-        }
-        dict_two = {
-            'Appointment_ID'            : '1234',
-            'Appointment_Start_Datetime': '2019-01-01 12:00:00'
-        }
-        appointment = Appointment(**dict_one)
-        self.assertFalse(loader.non_date_identical(appointment, dict_two))
-
-
 class SaveOrDiscardTestCase(OpalTestCase):
 
     def setUp(self):
