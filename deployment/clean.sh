7989e64c6f1684156a115f7db8c66f1cda9ec742
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-09-29 09:58:42 +0100

    Remove the clean.sh script which was there for testing purposes

--- deployment/clean.sh
+++
@@ -1,6 +0,0 @@
-docker stop rfh_ansible_container
-docker rm rfh_ansible_container
-docker rmi rfh_ansible_image
-sudo docker build -t rfh_ansible_image .
-docker run -d -P --name rfh_ansible_container rfh_ansible_image
-sudo docker port rfh_ansible_container
