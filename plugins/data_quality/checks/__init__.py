3db9565ecc5d5294243ce5c04ad9b7cf9bd90a2f
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-11-15 22:25:02 +0000

    WIP Duplicate patient checks, vpn is down so needs to be tested

--- plugins/data_quality/checks/__init__.py
+++ plugins/data_quality/checks/__init__.py
@@ -1,2 +1,8 @@
-daily = []
+from plugins.data_quality.checks import find_duplicates
+
+
+daily = [
+	find_duplicates.find_exact_duplicates,
+	find_duplicates.find_zero_leading_duplicates,
+]
 monthly = []
