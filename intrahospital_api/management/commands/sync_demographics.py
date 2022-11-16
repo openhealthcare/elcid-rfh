cc21790b786db0b3cc79295623930375567cb3c2
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-11-16 17:00:03 +0000

    Adds in checks for the sync demographics
    After the sync demographics runs once a day it checks to make sure we
    are matchking up

--- intrahospital_api/management/commands/sync_demographics.py
+++ intrahospital_api/management/commands/sync_demographics.py
@@ -3,7 +3,10 @@ A management command that is run by a cron job
 to run intrahospital_api.update_demographics.sync_recent_patient_information
 """
 import traceback
+import datetime
+
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 from intrahospital_api.update_demographics import sync_recent_patient_information
 from intrahospital_api import logger
 
@@ -14,6 +17,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             sync_recent_patient_information()
+            if datetime.datetime.now().hour == 0:
+                call_command('check_transfer_histories')
         except Exception:
             msg = f'Exception writing patient information \n {traceback.format_exc()}'
             logger.error(msg)
