670e0f413d49fdddac29e19063afd77780156443
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-11-21 09:50:52 +0000

    Adds a search backend so zero prefixes are stripped off before a query
    Before the fuzzy query we strip off any zero prefixes. This is so if
    they search for hn 0123 patient 123 will be returned.
    
    This is part of our move not to have any zero prefixed hospital numbers
    in elcid to make us match the master file table.

---
+++ plugins/elcid_search/elcid_query.py
@@ -0,0 +1,8 @@
+from opal.core.search.queries import DatabaseQuery
+
+
+class ElcidSearchQuery(DatabaseQuery):
+    def fuzzy_query(self):
+        query_parts = self.query.split(" ")
+        self.query = " ".join(i.lstrip('0') for i in query_parts)
+        return super().fuzzy_query()
