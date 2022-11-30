8d3b14ca05f60612718388fbff185aa33f96730e
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-11-30 21:28:59 +0000

    Changes the patient search summary to return previous MRNs for the patient
    Returns previous MRNs that have been merged into that patient in the
    patient summary search result

--- plugins/elcid_search/tests/test_elcid_query.py
+++ plugins/elcid_search/tests/test_elcid_query.py
@@ -1,3 +1,5 @@
+import json
+from django.urls import reverse
 from opal.core.test import OpalTestCase
 from plugins.elcid_search import elcid_query
 
@@ -32,3 +34,44 @@ class FuzzySearchIncludesMergedPatientsTestCase(OpalTestCase):
         query = elcid_query.ElcidSearchQuery(self.user, "456")
         patients = query.fuzzy_query()
         self.assertEqual(list(patients), [patient])
+
+
+class PatientSummaryTestCase(OpalTestCase):
+    def test_to_dict(self):
+        patient, episode = self.new_patient_and_episode_please()
+        patient.patientmerge_set.create(
+            mrn="234"
+        )
+        result = elcid_query.ElcidPatientSummary(
+            patient, [episode]
+        ).to_dict()
+        self.assertEqual(
+            result['previous_mrns'], ["234"]
+        )
+
+class IntegerationTestCase(OpalTestCase):
+    def test_integration(self):
+        """
+        An integration test to make sure we are implementing
+        the opal search api as it is expected.
+
+        It searches for a zero prefixed merged MRN
+        and returns a json structure the correct MRN
+        along with the merged url
+        """
+        patient, _ = self.new_patient_and_episode_please()
+        patient.demographics_set.update(
+            hospital_number="123"
+        )
+        patient.patientmerge_set.create(
+            mrn="234"
+        )
+        url = reverse('simple_search')
+        self.client.login(
+            username=self.user.username, password=self.PASSWORD
+        )
+        resp = self.client.get(url, data={"query": "0234"})
+        data = resp.json()
+        self.assertEqual(len(data["object_list"]), 1)
+        self.assertEqual(data["object_list"][0]["hospital_number"], "123")
+        self.assertEqual(data["object_list"][0]["previous_mrns"], ["234"])
