b5adfe1e496a196d89fa0084d73a9c6754028116
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-11-30 21:28:59 +0000

    Changes the patient search summary to return previous MRNs for the patient
    Returns previous MRNs that have been merged into that patient in the
    patient summary search result

--- plugins/elcid_search/elcid_query.py
+++ plugins/elcid_search/elcid_query.py
@@ -1,7 +1,26 @@
-from opal.core.search.queries import DatabaseQuery
+from opal.core.search.queries import DatabaseQuery, PatientSummary
+from elcid import models
+
+
+class ElcidPatientSummary(PatientSummary):
+    def to_dict(self):
+        """
+        Adds the previous mrns that have been associated with the
+        patient to the patient summary.
+        """
+        result = super().to_dict()
+        previous_merges = models.MergedMRN.objects.filter(
+            patient_id=self.patient_id
+        ).order_by(
+            '-upstream_merge_datetime'
+        )
+        result["previous_mrns"] = list(previous_merges.values_list('mrn', flat=True))
+        return result
 
 
 class ElcidSearchQuery(DatabaseQuery):
+    patient_summary_class = ElcidPatientSummary
+
     def fuzzy_query(self):
         """
         Strips the zeros off the beginning of every item in the query.
--- plugins/elcid_search/tests/test_elcid_query.py
+++ plugins/elcid_search/tests/test_elcid_query.py
@@ -1,3 +1,5 @@
+import json
+from django.urls import reverse
 from opal.core.test import OpalTestCase
 from plugins.elcid_search import elcid_query
 
@@ -15,3 +17,61 @@ class ElcidQueryTestCase(OpalTestCase):
         query = elcid_query.ElcidSearchQuery(self.user, "0123")
         patients = query.fuzzy_query()
         self.assertEqual(list(patients), [patient])
+
+
+class FuzzySearchIncludesMergedPatientsTestCase(OpalTestCase):
+    """
+    When we do a fuzzy search we should also search merged patients
+    """
+    def test_fuzzy_search_includes_merged_patients(self):
+        patient, _ = self.new_patient_and_episode_please()
+        patient.demographics_set.update(
+            hospital_number="123"
+        )
+        patient.mergedmrn_set.create(
+            mrn="456"
+        )
+        query = elcid_query.ElcidSearchQuery(self.user, "456")
+        patients = query.fuzzy_query()
+        self.assertEqual(list(patients), [patient])
+
+
+class PatientSummaryTestCase(OpalTestCase):
+    def test_to_dict(self):
+        patient, episode = self.new_patient_and_episode_please()
+        patient.mergedmrn_set.create(
+            mrn="234"
+        )
+        result = elcid_query.ElcidPatientSummary(
+            patient, [episode]
+        ).to_dict()
+        self.assertEqual(
+            result['previous_mrns'], ["234"]
+        )
+
+class IntegerationTestCase(OpalTestCase):
+    def test_integration(self):
+        """
+        An integration test to make sure we are implementing
+        the opal search api as it is expected.
+
+        It searches for a zero prefixed merged MRN
+        and returns a json structure the correct MRN
+        along with the merged url
+        """
+        patient, _ = self.new_patient_and_episode_please()
+        patient.demographics_set.update(
+            hospital_number="123"
+        )
+        patient.mergedmrn_set.create(
+            mrn="234"
+        )
+        url = reverse('simple_search')
+        self.client.login(
+            username=self.user.username, password=self.PASSWORD
+        )
+        resp = self.client.get(url, data={"query": "0234"})
+        data = resp.json()
+        self.assertEqual(len(data["object_list"]), 1)
+        self.assertEqual(data["object_list"][0]["hospital_number"], "123")
+        self.assertEqual(data["object_list"][0]["previous_mrns"], ["234"])
