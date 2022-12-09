bb7477c118c276c91b185a6266921286aef581d6
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-12-09 15:48:39 +0000

    Changes ipc.loader.load_all_extra_ipc_patients to look at merged mrns
    Changes the load_all_extra_ipc_patients to also check the merged MRN
    table before creating patients.

--- plugins/ipc/loader.py
+++ plugins/ipc/loader.py
@@ -1,7 +1,7 @@
 """
 Load utilities for the IPC module
 """
-from elcid.models import Demographics
+from elcid.models import Demographics, MergedMRN
 from elcid.episode_categories import InfectionService
 from intrahospital_api.apis.prod_api import ProdApi as ProdAPI
 from intrahospital_api.loader import create_rfh_patient_from_hospital_number
@@ -26,12 +26,22 @@ def load_all_extra_ipc_patients():
             Q_GET_DISTINCT_MRNS_FOR_TEST,
             params={'test_name': test.TEST_NAME}
         )
-        all_tested_mrns.update([r['Patient_Number'] for r in tested_mrns])
+        # elcid does not use zero prefixed MRNs but the lab test
+        # system does so strip off any zeros.
+        all_tested_mrns.update([
+            r['Patient_Number'].lstrip('0') for r in tested_mrns
+            if r['Patient_Number'].lstrip('0')
+        ])
 
-    all_mrns = set(
+    demographics_mrns = list(
         Demographics.objects.values_list('hospital_number', flat=True)
     )
 
+    merged_mrns = list(
+        MergedMRN.objects.values_list('mrn', flat=True)
+    )
+
+    all_mrns = set(demographics_mrns + merged_mrns)
     new_mrns = all_tested_mrns.difference(all_mrns)
     num_new = len(new_mrns)
     print(f"{num_new} New MRNs")
