239b7f4121c3ec96017ec42013ca38f8ed61d3e0
fredkingham <fred.kingham@openhealthcare.org.uk>
2023-02-03 11:28:33 +0000

    Converts tabs to spaces in the admissions.test_views

--- plugins/admissions/tests/test_views.py
+++ plugins/admissions/tests/test_views.py
@@ -3,27 +3,28 @@ from opal.core.test import OpalTestCase
 from plugins.admissions import detail
 from plugins.ipc import constants as ipc_constants
 
+
 class LocationHistoryViewVisibilityTestCase(OpalTestCase):
-	def setUp(self):
-		self.view_user = User.objects.create(
-			username="view_user"
-		)
+    def setUp(self):
+        self.view_user = User.objects.create(
+            username="view_user"
+        )
 
-	def test_superuser(self):
-		self.view_user.is_superuser = True
-		self.assertTrue(detail.LocationHistoryView.visible_to(
-			self.view_user
-		))
+    def test_superuser(self):
+        self.view_user.is_superuser = True
+        self.assertTrue(detail.LocationHistoryView.visible_to(
+            self.view_user
+        ))
 
-	def test_ipc(self):
-		self.view_user.profile.roles.create(
-			name=ipc_constants.IPC_ROLE
-		)
-		self.assertTrue(detail.LocationHistoryView.visible_to(
-			self.view_user
-		))
+    def test_ipc(self):
+        self.view_user.profile.roles.create(
+            name=ipc_constants.IPC_ROLE
+        )
+        self.assertTrue(detail.LocationHistoryView.visible_to(
+            self.view_user
+        ))
 
-	def test_other_user(self):
-		self.assertFalse(detail.LocationHistoryView.visible_to(
-			self.view_user
-		))
+    def test_other_user(self):
+        self.assertFalse(detail.LocationHistoryView.visible_to(
+            self.view_user
+        ))
