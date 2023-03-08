af120678e7079701f3fae037c8b01733bbfef307
fredkingham <fred.kingham@openhealthcare.org.uk>
2023-03-08 13:24:50 +0000

    Makes the user admin form error on duplicate email
    If a user is added with an existing email or if a user is edited with an
    existing email it should raise a Validation error.

--- elcid/admin.py
+++ elcid/admin.py
@@ -1,10 +1,24 @@
 """
 Admin for elCID models
 """
+from django.contrib.auth.models import User
 from django.contrib import admin
 from reversion.admin import VersionAdmin
+from opal import admin as opal_admin
+from django.contrib.auth.admin import UserAdmin
+from elcid import forms
 
 from elcid.models import BloodCultureIsolate
+
+
+admin.site.unregister(User)
+
+
+class ElcidUserAdmin(opal_admin.UserProfileAdmin):
+	form = forms.ElcidUserAdminChangeForm
+	add_form = forms.ElcidUserAdminCreationForm
+
+admin.site.register(User, ElcidUserAdmin)
 
 
 admin.site.register(BloodCultureIsolate, VersionAdmin)
--- elcid/forms.py
+++ elcid/forms.py
@@ -1,10 +1,44 @@
 """
 Forms for eLCID
 """
+from django.contrib.auth.models import User
 from django import forms
+from django.contrib.auth import forms as user_forms
 
 class BulkCreateUsersForm(forms.Form):
     """
     Form for uploading a CSV of users to add.
     """
     users = forms.FileField()
+
+
+class ElcidUserAdminCreationForm(user_forms.UserCreationForm):
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if len(email) == 0:
+            return email
+        existing_user = User.objects.filter(
+            email=email
+        ).first()
+        if existing_user is not None:
+            raise forms.ValidationError(
+                f"Email is currently in use by {existing_user.username}"
+            )
+        return email
+
+
+class ElcidUserAdminChangeForm(user_forms.UserChangeForm):
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if len(email) == 0:
+            return email
+        existing_user = User.objects.exclude(
+            id=self.instance.id
+        ).filter(
+            email=email
+        ).first()
+        if existing_user:
+            raise forms.ValidationError(
+                f"Email is currently in use by {existing_user.username}"
+            )
+        return email
