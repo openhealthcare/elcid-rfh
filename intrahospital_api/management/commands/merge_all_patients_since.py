d1edffe59774c97e5cd18740519ebddb9cfd3b46
fredkingham <fred.kingham@openhealthcare.org.uk>
2023-03-13 22:00:46 +0000

    Converts merge_all_patients_since to use spaces not tabs

--- intrahospital_api/management/commands/merge_all_patients_since.py
+++ intrahospital_api/management/commands/merge_all_patients_since.py
@@ -5,7 +5,7 @@ from intrahospital_api import update_demographics
 
 
 class Command(BaseCommand):
-	def handle(self, *args, **options):
-		since = timezone.now() - datetime.timedelta(2)
-		upstream_merged_mrns = update_demographics.get_all_merged_mrns_since(since)
-		update_demographics.check_and_handle_upstream_merges_for_mrns(upstream_merged_mrns)
+    def handle(self, *args, **options):
+        since = timezone.now() - datetime.timedelta(2)
+        upstream_merged_mrns = update_demographics.get_all_merged_mrns_since(since)
+        update_demographics.check_and_handle_upstream_merges_for_mrns(upstream_merged_mrns)
