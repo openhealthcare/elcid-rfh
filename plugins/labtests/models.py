3cf28a7ec93f564ceb19859c7ad6620656ec55ba
fredkingham <fred.kingham@openhealthcare.org.uk>
2023-03-18 08:57:19 +0000

    Allows none in the reference range

--- plugins/labtests/models.py
+++ plugins/labtests/models.py
@@ -201,6 +201,8 @@ class Observation(AbstractObserveration):
         Clean these and handle them appropriately
         For the moment we will now pass < 17
         """
+        if self.reference_range is None:
+            return
         reference_range = self.reference_range.replace("]", "").replace("[", "")
         regex = r"\s*([-+]?[0-9]+(\.[0-9]+)?)\s*-\s*([-+]?[0-9]+(\.[0-9]+)?)\s*?"
         matches = re.search(regex, reference_range)
