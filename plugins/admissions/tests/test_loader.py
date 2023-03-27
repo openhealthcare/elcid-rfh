e1d4ab5afd2302fbe08ceff483c2704e0e472e01
fredkingham <fred.kingham@openhealthcare.org.uk>
2023-03-27 15:14:12 +0100

    Adds unit tests for creating/merged transfer histories
    Make sure that in the batch we should create patients if they do not
    exist.

--- plugins/admissions/tests/test_loader.py
+++ plugins/admissions/tests/test_loader.py
@@ -2,6 +2,7 @@ import datetime
 from opal.core.test import OpalTestCase
 from django.utils import timezone
 from plugins.admissions import loader, models
+from opal.models import Patient
 from unittest.mock import patch
 from elcid import episode_categories
 
@@ -46,6 +47,76 @@ class TansferHistoriesTestCase(OpalTestCase):
         )
         self.assertEqual(
             found_th.transfer_end_datetime.date(), datetime.date.today() - datetime.timedelta(1)
+        )
+
+class CreateTransferHistoriesFromUpstreamResultTestCase(OpalTestCase):
+    @patch('intrahospital_api.loader.create_rfh_patient_from_hospital_number')
+    def test_creates_patients(self, create_rfh_patient_from_hospital_number):
+        row = {
+            k: None for k in models.TransferHistory.UPSTREAM_FIELDS_TO_MODEL_FIELDS.keys()
+        }
+        two_days_ago = datetime.datetime.now() - datetime.timedelta(2)
+        yesterday = datetime.datetime.now() - datetime.timedelta(1)
+        row["ENCNTR_SLICE_ID"] = 1231231
+        row["LOCAL_PATIENT_IDENTIFIER"] = "123"
+        row["SITE_CODE"] = "1231231"
+        row["ENCNTR_SLICE_ID"] = 1231231
+        row["UNIT"] = "X"
+        row["ROOM"] = "7W"
+        row["BED"] = "B12"
+        row["In_TransHist"] = 1
+        row["In_Spells"] = 1
+        row["TRANS_HIST_START_DT_TM"] = two_days_ago
+        row["TRANS_HIST_END_DT_TM"] = yesterday
+        def create_patient(*args, **kwargs):
+            patient, _ = self.new_patient_and_episode_please()
+            patient.demographics_set.update(hospital_number="123")
+            return patient
+        create_rfh_patient_from_hospital_number.side_effect = create_patient
+        loader.create_transfer_histories_from_upstream_result([row])
+        patient = Patient.objects.get(demographics__hospital_number="123")
+        found_th = patient.transferhistory_set.get()
+        self.assertEqual(found_th.encounter_slice_id, row["ENCNTR_SLICE_ID"])
+        self.assertEqual(found_th.site_code, row["SITE_CODE"])
+        self.assertEqual(found_th.unit, row["UNIT"])
+        self.assertEqual(found_th.room, row["ROOM"])
+        self.assertEqual(found_th.bed, row["BED"])
+        self.assertEqual(
+            found_th.transfer_start_datetime.date(), datetime.date.today() - datetime.timedelta(2)
+        )
+        self.assertEqual(
+            found_th.transfer_end_datetime.date(), datetime.date.today() - datetime.timedelta(1)
+        )
+
+    def handles_merged_mrns(self):
+        row = {
+            k: None for k in models.TransferHistory.UPSTREAM_FIELDS_TO_MODEL_FIELDS.keys()
+        }
+
+        two_days_ago = datetime.datetime.now() - datetime.timedelta(2)
+        yesterday = datetime.datetime.now() - datetime.timedelta(1)
+
+        row["ENCNTR_SLICE_ID"] = 1231231
+        row["LOCAL_PATIENT_IDENTIFIER"] = "234"
+        row["SITE_CODE"] = "1231231"
+        row["ENCNTR_SLICE_ID"] = 1231231
+        row["UNIT"] = "X"
+        row["ROOM"] = "7W"
+        row["BED"] = "B12"
+        row["In_TransHist"] = 1
+        row["In_Spells"] = 1
+        row["TRANS_HIST_START_DT_TM"] = two_days_ago
+        row["TRANS_HIST_END_DT_TM"] = yesterday
+        patient, _ = self.new_patient_and_episode_please()
+        patient.demographics_set.update(
+            hospital_number="123"
+        )
+        patient.mergedmrn_set.create(
+            mrn="234"
+        )
+        loader.create_transfer_histories_from_upstream_result([row])
+        self.assertTrue(
+            patient.transferhistory_set.exists()
         )
 
 
