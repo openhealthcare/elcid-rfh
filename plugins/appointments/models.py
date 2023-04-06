8aab770bf6ce381bde5b40c765cf901ac60fc940
fredkingham <fred.kingham@openhealthcare.org.uk>
2023-04-06 17:32:47 +0100

    Fixes the rest of the casing in the appontments

--- plugins/appointments/models.py
+++ plugins/appointments/models.py
@@ -80,7 +80,7 @@ class Appointment(models.Model):
     aig_resource_id          = models.CharField(
         max_length=255,blank=True, null=True,
         help_text="General Resourse associated with non-consultant seeing the patient eg SPR, Nurse, Therapist")
-    aiL_location_resource_id = models.CharField(max_length=255,blank=True, null=True,
+    ail_location_resource_id = models.CharField(max_length=255,blank=True, null=True,
                                                 help_text="Actual location of the appointment")
     aip_personnel_id         = models.CharField(max_length=255, blank=True, null=True,
                                                 help_text="Consultant resource details if they are seeing the patient.")
