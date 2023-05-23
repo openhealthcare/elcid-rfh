60c6e088f9209965b325cb77ba0a1afb16f0599a
fredkingham <fred.kingham@openhealthcare.org.uk>
2023-05-23 16:53:21 +0100

    Adds the patient's name into the no mdt action header

--- plugins/tb/static/js/tb/controllers/tb_mdt_no_action.js
+++ plugins/tb/static/js/tb/controllers/tb_mdt_no_action.js
@@ -1,6 +1,7 @@
 angular.module('opal.controllers').controller(
     'TBMDTNoActionCtrl',
-    function($scope, $modalInstance, $http, patient_id) {
+    function($scope, $modalInstance, $http, patient_id, name) {
+        $scope.name = name;
         $scope.no_action = function(what){
             $http.put('/api/v0.1/tb_mdt_no_action/' + patient_id + '/',
                       {}).then(
