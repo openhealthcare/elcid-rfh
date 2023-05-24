34c45d132d439ca02cfca9d784268fba637a9a74
fredkingham <fred.kingham@openhealthcare.org.uk>
2023-05-24 17:20:14 +0100

    Bumps the RFH version to 0.110

--- elcid/settings.py
+++ elcid/settings.py
@@ -419,7 +419,7 @@ else:
     EMAIL_HOST = 'localhost'
 
 
-VERSION_NUMBER = '0.109'
+VERSION_NUMBER = '0.110'
 
 #TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #TEST_RUNNER = 'django_test_coverage.runner.CoverageTestSuiteRunner'
