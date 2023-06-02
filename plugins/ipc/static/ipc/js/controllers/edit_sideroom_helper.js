38f7b36ed90f615dac4a448cb81ea2ed329951b8
David Miller <david@deadpansincerity.com>
2023-06-02 11:27:54 +0100

    Make sideroom status editable from the sideroom list

---
+++ plugins/ipc/static/ipc/js/controllers/edit_sideroom_helper.js
@@ -0,0 +1,16 @@
+angular.module(
+    'opal.controllers').controller(
+        'EditSideroomHelper',
+	function($scope, $http, patientLoader){
+	    var self = this;
+
+            this.edit_status = function(patient_id){
+                patientLoader(patient_id).then(function(patient){
+                    patient.recordEditor.editItem(
+                        'sideroom_status', patient.sideroom_status[0]
+                    ).then(function(){
+                        window.location.reload();
+                    });
+                })
+            }
+        });
