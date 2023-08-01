78e26f0a50182de5821d946639cf327588f2b516
David Miller <david@deadpansincerity.com>
2023-08-01 13:23:14 +0100

    Update version number

--- elcid/settings.py
+++ elcid/settings.py
@@ -419,7 +419,7 @@ else:
     EMAIL_HOST = 'localhost'
 
 
-VERSION_NUMBER = '0.111'
+VERSION_NUMBER = '0.112'
 
 #TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #TEST_RUNNER = 'django_test_coverage.runner.CoverageTestSuiteRunner'
