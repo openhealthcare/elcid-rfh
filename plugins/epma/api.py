8a6d28e4c35b4af2d683406468f37a7832112c36
David Miller <david@openhealthcare.org.uk>
2025-06-03 08:27:40 +0100

    Second filtered list

--- plugins/epma/api.py
+++ plugins/epma/api.py
@@ -6,7 +6,7 @@ from opal.core.api import LoginRequiredViewset
 from opal.core.views import json_response
 from opal.models import Patient
 
-from plugins.epma.models import EPMAMedOrder
+from plugins.epma.models import EPMAMedOrder, EPMATherapeuticClassLookup
 
 
 class EPMAViewSet(LoginRequiredViewset):
@@ -15,4 +15,15 @@ class EPMAViewSet(LoginRequiredViewset):
     def retrieve(self, request, pk):
         patient = get_object_or_404(Patient.objects.all(), pk=pk)
         orders  = EPMAMedOrder.objects.filter(patient=patient).order_by('-o_start_dt_tm')
-        return json_response([o.to_dict() for o in orders])
+
+        identifiers = EPMATherapeuticClassLookup.objects.filter(
+            multum_hierarchy_1='anti-infectives').values_list(
+                'mcdx_drug_identifier', flat=True
+            )
+
+        anti_infectives = [o for o in orders if o.drug_identifier in identifiers]
+
+        return json_response({
+            'all': [o.to_dict() for o in orders],
+            'anti_infectives': anti_infectives
+        })
