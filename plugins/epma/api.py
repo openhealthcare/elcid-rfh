5262754b1898453c28ba4c116cd473a2e330b9e5
David Miller <david@openhealthcare.org.uk>
2025-07-02 10:19:13 +0100

    Fixup: typo

--- plugins/epma/api.py
+++ plugins/epma/api.py
@@ -4,7 +4,7 @@ APIs for EPMA data
 from django.shortcuts import get_object_or_404
 from opal.core.api import LoginRequiredViewset
 from opal.core.views import json_response
-from django.db.models import Q
+
 from opal.models import Patient
 
 from plugins.epma.models import EPMAMedOrder, EPMATherapeuticClassLookup
@@ -15,7 +15,7 @@ class EPMAViewSet(LoginRequiredViewset):
 
     def retrieve(self, request, pk):
         patient = get_object_or_404(Patient.objects.all(), pk=pk)
-        orders  = EPMAMedOrder.objects.filter(patient=patient).order_by('-o_start_dt_tm')-
+        orders  = EPMAMedOrder.objects.filter(patient=patient).order_by('-o_start_dt_tm')
 
         identifiers = EPMATherapeuticClassLookup.objects.filter(
             Q(multum_hierarchy_1='anti-infectives') | Q(multum_hierarchy_2='anti-infectives')).values_list(
