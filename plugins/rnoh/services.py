b7002ec4a18b4161f139efe5b5767d975d7a6c75
David Miller <david@openhealthcare.org.uk>
2025-08-26 14:09:14 +0100

    Make RNOH global

--- plugins/rnoh/services.py
+++ plugins/rnoh/services.py
@@ -9,6 +9,11 @@ from elcid.services import ClinicalService
 class RNOHService(ClinicalService):
 
     @classmethod
+    def visible_to(klass, user):
+        return True
+
+
+    @classmethod
     def as_menuitem(klass):
         return menus.MenuItem(
             href='/#/rnoh/inpatients/',
