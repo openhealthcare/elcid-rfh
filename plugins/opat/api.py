5a2b3ff3cdea9112f07c366752156150aa017863
David Miller <david@openhealthcare.org.uk>
2025-08-27 09:22:36 +0100

    Joins OPAT API to summary function

--- plugins/opat/api.py
+++ plugins/opat/api.py
@@ -11,5 +11,4 @@ class OPATTestSummaryAPI(LoginRequiredViewset):
     @patient_from_pk
     def retrieve(self, request, patient):
         opat_summary_information = get_opat_summmary_information(patient)
-        result = {'your':'data'}
-        return json_response(result)
+        return json_response(opat_summary_information)
