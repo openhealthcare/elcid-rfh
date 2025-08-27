316d74bd0ad20921f033b4b1ee7ffed4bed7183d
David Miller <david@openhealthcare.org.uk>
2025-08-27 09:28:21 +0100

    Dates to strings for serializing

--- plugins/opat/utils.py
+++ plugins/opat/utils.py
@@ -72,7 +72,7 @@ def get_opat_summmary_information(patient):
             obs_name = obs.observation_name
             if obs_name in RELEVANT_OBS_NAMES[test.test_name]:
 
-                as_date = obs.observation_datetime.date()
+                as_date = obs.observation_datetime.date().strftime("%d/%m/%Y")
 
                 # Sometimes we get complaints from the lab
                 if obs.observation_value.startswith('Regret') == False:
