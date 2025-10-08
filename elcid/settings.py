0d469f73518c5b01e57837de2188bf1ca3588d01
David Miller <david@openhealthcare.org.uk>
2025-10-08 22:08:21 +0100

    Version number bump

--- elcid/settings.py
+++ elcid/settings.py
@@ -419,7 +419,7 @@ else:
     EMAIL_HOST = 'localhost'
 
 
-VERSION_NUMBER = '0.130'
+VERSION_NUMBER = '0.131'
 
 #TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #TEST_RUNNER = 'django_test_coverage.runner.CoverageTestSuiteRunner'
