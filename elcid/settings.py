00f2985b9fda7a8477f26e2846ad88b09fa68de3
David Miller <david@openhealthcare.org.uk>
2026-01-12 13:26:06 +0000

    Order by start date in RNOH template for treatment

--- elcid/settings.py
+++ elcid/settings.py
@@ -419,7 +419,7 @@ else:
     EMAIL_HOST = 'localhost'
 
 
-VERSION_NUMBER = '0.136'
+VERSION_NUMBER = '0.137'
 
 #TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #TEST_RUNNER = 'django_test_coverage.runner.CoverageTestSuiteRunner'
