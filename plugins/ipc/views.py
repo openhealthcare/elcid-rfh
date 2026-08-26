02941d995e1948781d4837711bd61d8ffd0e3217
David Miller <david@openhealthcare.org.uk>
2026-08-26 09:55:41 +0100

    Typo: var access

--- plugins/ipc/views.py
+++ plugins/ipc/views.py
@@ -359,8 +359,7 @@ class SideroomSummaryView(SideRoomView):
                         )
                     )
 
-
-                if status.is_open_bay:
+                if status.getatr('is_open_bay', False):
                     self.open_bay += 1
 
                 if any([status.room.startswith('SR'), status.bed.startswith('SR')]):
